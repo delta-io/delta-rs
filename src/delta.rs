d6f0d7e69f6ef6b11267cc456c33eb8c646c5fd6
Qingping Hou <dave2008713@gmail.com>
2020-05-06 22:44:37 -0700

    add support for datafusion integration

--- src/delta.rs
+++ src/delta.rs
@@ -12,6 +12,7 @@ use regex::Regex;
 use std::collections::HashMap;
 use std::fmt;
 use std::io::{BufRead, BufReader, Cursor};
+use std::path::Path;
 
 use thiserror;
 
@@ -46,21 +47,45 @@ pub struct Format {
 pub struct SchemaTypeStruct {
     // type field is alwsy the string "struct", so we are ignoring it here
     r#type: String,
-    fields: Vec<SchemaTypeStructField>,
+    fields: Vec<SchemaField>,
+}
+
+impl SchemaTypeStruct {
+    pub fn get_fields(&self) -> &Vec<SchemaField> {
+        &self.fields
+    }
 }
 
 #[derive(Serialize, Deserialize, Debug)]
-pub struct SchemaTypeStructField {
+pub struct SchemaField {
     // Name of this (possibly nested) column
     name: String,
     // String containing the name of a primitive type, a struct definition, an array definition or
     // a map definition
-    r#type: SchemaType,
+    r#type: SchemaDataType,
     // Boolean denoting whether this field can be null
     nullable: bool,
     // A JSON map containing information about this column. Keys prefixed with Delta are reserved
     // for the implementation.
     metadata: HashMap<String, String>,
+}
+
+impl SchemaField {
+    pub fn get_name(&self) -> &str {
+        &self.name
+    }
+
+    pub fn get_type(&self) -> &SchemaDataType {
+        &self.r#type
+    }
+
+    pub fn is_nullable(&self) -> bool {
+        self.nullable
+    }
+
+    pub fn get_metadata(&self) -> &HashMap<String, String> {
+        &self.metadata
+    }
 }
 
 #[derive(Serialize, Deserialize, Debug)]
@@ -69,9 +94,19 @@ pub struct SchemaTypeArray {
     r#type: String,
     // The type of element stored in this array represented as a string containing the name of a
     // primitive type, a struct definition, an array definition or a map definition
-    elementType: Box<SchemaType>,
+    elementType: Box<SchemaDataType>,
     // Boolean denoting whether this array can contain one or more null values
     containsNull: bool,
+}
+
+impl SchemaTypeArray {
+    pub fn get_element_type(&self) -> &SchemaDataType {
+        &self.elementType
+    }
+
+    pub fn does_contain_null(&self) -> bool {
+        self.containsNull
+    }
 }
 
 #[derive(Serialize, Deserialize, Debug)]
@@ -79,15 +114,39 @@ pub struct SchemaTypeMap {
     r#type: String,
     // The type of element used for the key of this map, represented as a string containing the
     // name of a primitive type, a struct definition, an array definition or a map definition
-    keyType: Box<SchemaType>,
+    keyType: Box<SchemaDataType>,
     // The type of element used for the key of this map, represented as a string containing the
     // name of a primitive type, a struct definition, an array definition or a map definition
-    valueType: Box<SchemaType>,
+    valueType: Box<SchemaDataType>,
 }
 
+impl SchemaTypeMap {
+    pub fn get_key_type(&self) -> &SchemaDataType {
+        &self.keyType
+    }
+
+    pub fn get_value_type(&self) -> &SchemaDataType {
+        &self.valueType
+    }
+}
+
+/*
+ * List of primitive types:
+ *   string: utf8
+ *   long  // undocumented, i64?
+ *   integer: i32
+ *   short: i16
+ *   byte: i8
+ *   float: f32
+ *   double: f64
+ *   boolean: bool
+ *   binary: a sequence of binary data
+ *   date: A calendar date, represented as a year-month-day triple without a timezone
+ *   timestamp: Microsecond precision timestamp without a timezone
+ */
 #[derive(Serialize, Deserialize, Debug)]
 #[serde(untagged)]
-pub enum SchemaType {
+pub enum SchemaDataType {
     primitive(String),
     r#struct(SchemaTypeStruct),
     array(SchemaTypeArray),
@@ -97,7 +156,13 @@ pub enum SchemaType {
 #[derive(Serialize, Deserialize, Debug)]
 pub struct Schema {
     r#type: String,
-    fields: Vec<SchemaTypeStructField>,
+    fields: Vec<SchemaField>,
+}
+
+impl Schema {
+    pub fn get_fields(&self) -> &Vec<SchemaField> {
+        &self.fields
+    }
 }
 
 fn populate_hashmap_from_parquet_map(
@@ -811,6 +876,27 @@ impl DeltaTable {
 
     pub fn get_files(&self) -> &Vec<String> {
         &self.files
+    }
+
+    pub fn get_file_paths(&self) -> Vec<String> {
+        self.files
+            .iter()
+            .map(|fname| {
+                let table_path = Path::new(&self.table_path);
+                table_path
+                    .join(fname)
+                    .into_os_string()
+                    .into_string()
+                    .unwrap()
+            })
+            .collect()
+    }
+
+    pub fn schema(&self) -> Option<&Schema> {
+        match &self.current_metadata {
+            Some(meta) => Some(&meta.schema),
+            None => None,
+        }
     }
 
     pub fn new(
--- src/lib.rs
+++ src/lib.rs
@@ -2,12 +2,14 @@
 extern crate log;
 
 extern crate arrow;
+extern crate datafusion;
 extern crate rust_dataframe;
 
-mod dataframe;
 mod delta;
+mod delta_dataframe;
+mod delta_datafusion;
 mod storage;
 
-pub use self::dataframe::*;
 pub use self::delta::*;
+pub use self::delta_dataframe::*;
 pub use self::storage::*;
