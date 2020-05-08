ee7bb316770d3d409da7b5b19dbae9dc35de6aba
R. Tyler Croy <rtyler@brokenco.de>
2020-05-08 11:54:45 -0700

    Checkpoint work in progress on implementing the deltalake-ruby crate
    Need to checkpoint this here to rebase and get some work merged before more
    delta crate changes happen

--- ruby/lib/deltalake.rb
+++ ruby/lib/deltalake.rb
@@ -2,4 +2,7 @@ require 'helix_runtime'
 require 'deltalake/native'
 
 module Deltalake
+  def self.open_table(table_path)
+    Table.new(table_path)
+  end
 end
--- ruby/src/lib.rs
+++ ruby/src/lib.rs
@@ -2,6 +2,32 @@
 
 #[macro_use]
 extern crate helix;
+extern crate delta;
+
+use delta::DeltaTable;
+use std::sync::Arc;
+
 
 ruby! {
+    class Table {
+        struct {
+            table_path: String,
+            // TODO
+            // actual: Arc<DeltaTable>,
+        }
+
+        def initialize(helix, table_path: String) {
+            println!("initializing with {}", table_path);
+
+            let table = delta::open_table(&table_path).unwrap();
+            let actual = Arc::new(table);
+
+            Table {
+                helix,
+                table_path,
+                // TODO
+                // actual,
+            }
+        }
+    }
 }
