21dbbe06b0ece4422b2e0c6a3467ddba451d1cdc
R. Tyler Croy <rtyler@brokenco.de>
2020-05-08 11:54:45 -0700

    Checkpoint work in progress on implementing the deltalake-ruby crate
    Need to checkpoint this here to rebase and get some work merged before more
    delta crate changes happen

--- ruby/spec/deltalake_spec.rb
+++ ruby/spec/deltalake_spec.rb
@@ -1,4 +1,14 @@
+require 'spec_helper'
 require 'deltalake'
 
 describe Deltalake do
+  describe '#open_table' do
+    let(:table_path) do
+      File.expand_path('../../rust/tests/data/simple_table')
+    end
+
+    subject(:table) { Deltalake.open_table(table_path) }
+
+    its(:version) { should eq 4 }
+  end
 end
