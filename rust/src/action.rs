4f57ef315e44c0811afd61c747c2bd6cdadf4bb4
Qingping Hou <dave2008713@gmail.com>
2020-05-14 21:20:11 -0700

    add checkpoint test table
    also fixed time travel checkpoint handling bug

--- rust/src/action.rs
+++ rust/src/action.rs
@@ -486,3 +486,24 @@ impl Action {
         });
     }
 }
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    use parquet::file::reader::{FileReader, SerializedFileReader};
+    use std::fs::File;
+
+    #[test]
+    fn test_add_action_without_partition_values_and_stats() {
+        let path = "./tests/data/delta-0.2.0/_delta_log/00000000000000000003.checkpoint.parquet";
+        let preader = SerializedFileReader::new(File::open(path).unwrap()).unwrap();
+
+        let mut iter = preader.get_row_iter(None).unwrap();
+        let record = iter.nth(9).unwrap();
+        let add_record = record.get_group(1).unwrap();
+        let add_action = Add::from_parquet_record(&add_record).unwrap();
+
+        assert_eq!(add_action.partitionValues.len(), 0);
+        assert_eq!(add_action.stats, None);
+    }
+}
