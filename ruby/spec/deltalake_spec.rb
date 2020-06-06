f5e90a9022d32d68066b43a0709a9a4aa9d10106
R. Tyler Croy <rtyler@brokenco.de>
2020-06-05 21:46:45 -0700

    Wire some preliminary methods through from Ruby into Rust
    This is still early work, but it's a start.

--- ruby/spec/deltalake_spec.rb
+++ ruby/spec/deltalake_spec.rb
@@ -4,9 +4,19 @@ require 'deltalake'
 describe Deltalake do
   describe '#open_table' do
     let(:table_path) do
-      File.expand_path('../../rust/tests/data/simple_table')
+      File.expand_path('../rust/tests/data/simple_table')
     end
 
     subject(:table) { Deltalake.open_table(table_path) }
+
+    its(:table_path) { should eq(table_path) }
+    its(:version) { should eq 4 }
+
+    describe '#files' do
+      subject(:files) { table.files }
+
+      it { should be_instance_of Array }
+      its(:size) { should eq(5) }
+    end
   end
 end
