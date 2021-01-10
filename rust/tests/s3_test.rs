b3581ee06eee26d971bd3b76bb788c85ecf0c6c0
R. Tyler Croy <rtyler@brokenco.de>
2021-01-10 13:03:47 -0800

    Introduce a simple S3 integration test for reading from a table
    This IAM user basically has all Read/List permissions on the deltars bucket in
    an empty account I control.
    
    Fixes #36

---
+++ rust/tests/s3_test.rs
@@ -0,0 +1,44 @@
+#[cfg(feature = "s3")]
+mod s3 {
+    /*
+     * The S3 bucket used below resides in @rtyler's personal AWS account
+     *
+     * Should there be test failures, or if you need more files uploaded into this account, let him
+     * know
+     */
+    #[tokio::test]
+    async fn test_s3_simple() {
+        std::env::set_var("AWS_REGION", "us-west-2");
+        std::env::set_var("AWS_ACCESS_KEY_ID", "AKIAX7EGEQ7FT6CLQGWH");
+        std::env::set_var("AWS_SECRET_ACCESS_KEY", "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");
+        let table =
+            deltalake::open_table("s3://deltars/simple")
+                .await
+                .unwrap();
+        println!("{}", table);
+        assert_eq!(table.version, 4);
+        assert_eq!(table.min_writer_version, 2);
+        assert_eq!(table.min_reader_version, 1);
+        assert_eq!(
+            table.get_files(),
+            &vec![
+                "part-00000-c1777d7d-89d9-4790-b38a-6ee7e24456b1-c000.snappy.parquet",
+                "part-00001-7891c33d-cedc-47c3-88a6-abcfb049d3b4-c000.snappy.parquet",
+                "part-00004-315835fe-fb44-4562-98f6-5e6cfa3ae45d-c000.snappy.parquet",
+                "part-00007-3a0e4727-de0d-41b6-81ef-5223cf40f025-c000.snappy.parquet",
+                "part-00000-2befed33-c358-4768-a43c-3eda0d2a499d-c000.snappy.parquet",
+            ]
+        );
+        let tombstones = table.get_tombstones();
+        assert_eq!(tombstones.len(), 31);
+        assert_eq!(
+            tombstones[0],
+            deltalake::action::Remove {
+                path: "part-00006-63ce9deb-bc0f-482d-b9a1-7e717b67f294-c000.snappy.parquet"
+                    .to_string(),
+                deletionTimestamp: 1587968596250,
+                dataChange: true
+            }
+        );
+    }
+}
