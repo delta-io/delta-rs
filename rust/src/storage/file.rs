3e99bdfebd910b97fd79fc4474e11a92e1a6b44c
mosyp <mykhailo.osypov@gmail.com>
2021-03-15 13:48:28 +0200

    Add platform/file-system specific rename with flags call

--- rust/src/storage/file.rs
+++ rust/src/storage/file.rs
@@ -3,6 +3,8 @@ use std::pin::Pin;
 
 use chrono::DateTime;
 use futures::{Stream, TryStreamExt};
+use libc::{c_char, c_int, c_uint};
+use std::ffi::CString;
 use tokio::fs;
 use tokio::io::AsyncWriteExt;
 use tokio_stream::wrappers::ReadDirStream;
@@ -10,11 +12,15 @@ use tokio_stream::wrappers::ReadDirStream;
 use super::{ObjectMeta, StorageBackend, StorageError};
 
 #[derive(Default, Debug)]
-pub struct FileStorageBackend {}
+pub struct FileStorageBackend {
+    root: String,
+}
 
 impl FileStorageBackend {
-    pub fn new() -> Self {
-        Self {}
+    pub fn new(root: &str) -> Self {
+        Self {
+            root: String::from(root),
+        }
     }
 }
 
@@ -79,5 +85,89 @@ impl StorageBackend for FileStorageBackend {
         f.write(obj_bytes).await?;
 
         Ok(())
+    }
+}
+
+#[cfg(target_os = "linux")]
+const RENAME_NOREPLACE: c_uint = 1;
+
+#[cfg(target_os = "macos")]
+const RENAME_EXCL: c_uint = 4;
+
+extern "C" {
+    #[cfg(target_os = "linux")]
+    fn renameat2(
+        olddirfd: c_int,
+        oldpath: *const c_char,
+        newdirfd: c_int,
+        newpath: *const c_char,
+        flags: c_uint,
+    ) -> c_int;
+
+    #[cfg(target_os = "macos")]
+    fn renamex_np(from: *const c_char, to: *const c_char, flags: c_uint) -> c_int;
+}
+
+fn rename(from: &str, to: &str) -> Result<(), StorageError> {
+    let from_s = CString::new(from).unwrap();
+    let to_s = CString::new(to).unwrap();
+
+    unsafe {
+        cfg_if::cfg_if! {
+            if #[cfg(target_os = "linux")] {
+                if renameat2(0, from_s.as_ptr(), 0, to_s.as_ptr(), RENAME_NOREPLACE) == 0 {
+                   return Ok(())
+                }
+            } else if #[cfg(target_os = "macos")] {
+                if renamex_np(from_s.as_ptr(), to_s.as_ptr(), RENAME_EXCL) == 0 {
+                   return Ok(())
+                }
+            } else {
+                return Err(StorageError::FileSystemNotSupported)
+            }
+        }
+    }
+
+    return Err(StorageError::AlreadyExists(to.to_string()));
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    use std::fs::File;
+    use std::io::Write;
+    use std::path::PathBuf;
+    use uuid::Uuid;
+
+    #[test]
+    fn test_rename() {
+        let tmp = std::env::temp_dir().join(Uuid::new_v4().to_string());
+        std::fs::create_dir_all(&tmp).unwrap();
+        let a = create_file(&tmp, "a");
+        let b = create_file(&tmp, "b");
+        let c = tmp.join("c");
+
+        assert_eq!(a.exists(), true);
+        assert_eq!(c.exists(), false);
+        rename(a.to_str().unwrap(), c.to_str().unwrap()).unwrap();
+        assert_eq!(a.exists(), false);
+        assert_eq!(c.exists(), true);
+
+        assert_eq!(b.exists(), true);
+        match rename(b.to_str().unwrap(), c.to_str().unwrap()) {
+            Err(StorageError::AlreadyExists(p)) => assert_eq!(p, c.to_str().unwrap()),
+            _ => panic!("unexpected"),
+        }
+        assert_eq!(b.exists(), true);
+        assert_eq!(std::fs::read_to_string(c).unwrap(), "a");
+
+        std::fs::remove_dir_all(&tmp).unwrap();
+    }
+
+    fn create_file(dir: &Path, name: &str) -> PathBuf {
+        let path = dir.join(name);
+        let mut file = File::create(&path).unwrap();
+        file.write_all(name.as_bytes()).unwrap();
+        path
     }
 }
--- rust/src/storage/mod.rs
+++ rust/src/storage/mod.rs
@@ -157,6 +157,8 @@ pub enum StorageError {
     AlreadyExists(String),
     #[error("Failed to read local object content: {source}")]
     Io { source: std::io::Error },
+    #[error("File system not supported")]
+    FileSystemNotSupported,
 
     #[cfg(feature = "s3")]
     #[error("Failed to read S3 object content: {source}")]
@@ -293,7 +295,7 @@ pub trait StorageBackend: Send + Sync + Debug {
 
 pub fn get_backend_for_uri(uri: &str) -> Result<Box<dyn StorageBackend>, StorageError> {
     match parse_uri(uri)? {
-        Uri::LocalPath(_) => Ok(Box::new(file::FileStorageBackend::new())),
+        Uri::LocalPath(root) => Ok(Box::new(file::FileStorageBackend::new(root))),
         #[cfg(feature = "s3")]
         Uri::S3Object(_) => Ok(Box::new(s3::S3StorageBackend::new())),
         #[cfg(feature = "azure")]
