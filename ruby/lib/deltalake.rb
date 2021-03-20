e701c5b55111d746ac4d5e870c55dea5e7fbf485
Francis Murillo <francis.murillo@protonmail.com>
2021-03-20 10:51:53 +0800

    Migrate Ruby bindings from helix to rutie

--- ruby/lib/deltalake.rb
+++ ruby/lib/deltalake.rb
@@ -1,7 +1,11 @@
-require 'helix_runtime'
-require 'deltalake-ruby/native'
+# frozen_string_literal: true
+
+require 'deltalake/version'
+require 'rutie'
 
 module Deltalake
+  Rutie.new(:deltalake_ruby, lib_path: '../../target/release').init 'Init_table', __dir__
+
   def self.open_table(table_path)
     Table.new(table_path)
   end
---
+++ ruby/lib/deltalake/version.rb
@@ -0,0 +1,5 @@
+# frozen_string_literal: true
+
+module Deltalake
+  VERSION = '0.1.0'
+end
