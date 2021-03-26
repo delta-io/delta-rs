606ad40f6cd6ec54af7b3552acac2122d4f20e1b
Florian Valeye <florian.valeye@gmail.com>
2021-03-26 13:30:54 +0100

    Add documentation on Python binding

--- python/deltalake/table.py
+++ python/deltalake/table.py
@@ -10,16 +10,31 @@ from .schema import Schema, pyarrow_schema_from_json
 
 class DeltaTable:
     def __init__(self, table_path: str, version: Optional[int] = None):
+        """
+        Create the Delta Table from a path with an optional version.
+        Multiple StorageBackends are currently supported: AWS S3, Azure Data Lake Storage Gen2 and local URI
+        :param table_path: the path of the DeltaTable
+        :param version: version of the DeltaTable
+        """
         self._table = RawDeltaTable(table_path, version=version)
 
     def version(self) -> int:
+        """
+        Get the version of the DeltaTable.
+        :return: The current version of the DeltaTable
+        """
         return self._table.version()
 
     def files(self) -> List[str]:
+        """
+        Get the .parquet files of the DeltaTable.
+        :return: list of the .parquet files referenced for the current version of the DeltaTable
+        """
         return self._table.files()
 
     def files_by_partitions(self, partition_filters: List[Tuple]) -> List[str]:
         """
+        Get the files that matched after applying a list of partitions filters.
         Partitions which do not match the filter predicate will be removed from scanned data.
         Predicates are expressed in disjunctive normal form (DNF), like [("x", "=", "a"), ...].
         DNF allows arbitrary boolean logical combinations of single partition predicates.
@@ -36,6 +51,9 @@ class DeltaTable:
         ("x", "!=", "a")
         ("y", "in", ["a", "b", "c"])
         ("z", "not in", ["a","b"])
+        :param partition_filters: the partition filters that will be used for getting the matched files
+        :return: list of the .parquet files after applying the partition filters referenced for the current version of
+        the DeltaTable
         """
         try:
             return self._table.files_by_partitions(partition_filters)
@@ -45,18 +63,38 @@ class DeltaTable:
             )
 
     def file_paths(self) -> List[str]:
+        """
+        Get the list of files with an absolute path.
+        :return: list of the .parquet files with an absolute path referenced for the current version of the DeltaTable
+        """
         return self._table.file_paths()
 
     def load_version(self, version: int) -> None:
+        """
+        Load a DeltaTable with a specified version.
+        :param version: the identifier of the version of the DeltaTable to load
+        """
         self._table.load_version(version)
 
     def schema(self) -> Schema:
+        """
+        Get the current schema of the DeltaTable
+        :return: the current Schema registered in the transaction log
+        """
         return Schema.from_json(self._table.schema_json())
 
     def pyarrow_schema(self) -> pyarrow.Schema:
+        """
+        Get the current schema of the DeltaTable with the Parquet PyArrow format.
+        :return: the current Schema with the Parquet PyArrow format
+        """
         return pyarrow_schema_from_json(self._table.arrow_schema_json())
 
     def to_pyarrow_dataset(self) -> pyarrow.dataset.Dataset:
+        """
+        Read the DeltaTable with the PyArrow dataset format.
+        :return: the PyArrow dataset in PyArrow
+        """
         file_paths = self._table.file_paths()
         paths = [urlparse(curr_file) for curr_file in file_paths]
 
@@ -68,4 +106,8 @@ class DeltaTable:
             return dataset(file_paths, format="parquet")
 
     def to_pyarrow_table(self) -> pyarrow.Table:
+        """
+        Read the DeltaTable with the PyArrow Table format.
+        :return: the PyArrow table
+        """
         return self.to_pyarrow_dataset().to_table()
