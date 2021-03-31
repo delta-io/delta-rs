bdd2dd5c83aca1671b3a54c4ece3eb838a4217a5
Christian Williams <christianw@scribd.com>
2021-03-31 18:28:14 -0400

    Rustdocs for schema.rs (#178)
    * Add rustdocs to delta.rs
    
    * Add rustdocs to most of actions.rs
    
    * fix typo
    
    * Add most rustdocs under src/storage
    
    * Add rustdocs to schema.rs
    
    * Remove rustdoc from private fields
    
    * Remove rustdoc from private fields
    
    * Add enum rustdocs back in schema.rs
    
    * Correction in rustdocs for get_value_type

--- rust/src/schema.rs
+++ rust/src/schema.rs
@@ -4,12 +4,18 @@ use std::collections::HashMap;
 
 use serde::{Deserialize, Serialize};
 
+/// Type alias for a string expected to match a GUID/UUID format
 pub type Guid = String;
+/// Type alias for i64/Delta long
 pub type DeltaDataTypeLong = i64;
+/// Type alias representing the expected type (i64) of a Delta table version.
 pub type DeltaDataTypeVersion = DeltaDataTypeLong;
+/// Type alias representing the expected type (i64/ms since Unix epoch) of a Delta timestamp.
 pub type DeltaDataTypeTimestamp = DeltaDataTypeLong;
+/// Type alias for i32/Delta int
 pub type DeltaDataTypeInt = i32;
 
+/// Represents a struct field defined in the Delta table schema.
 // https://github.com/delta-io/delta/blob/master/PROTOCOL.md#Schema-Serialization-Format
 #[derive(Serialize, Deserialize, Debug, Default, Clone)]
 pub struct SchemaTypeStruct {
@@ -19,17 +25,17 @@ pub struct SchemaTypeStruct {
 }
 
 impl SchemaTypeStruct {
+    /// Returns the list of fields contained within the column struct.
     pub fn get_fields(&self) -> &Vec<SchemaField> {
         &self.fields
     }
 }
 
+/// Describes a specific field of the Delta table schema.
 #[derive(Serialize, Deserialize, Debug, Clone)]
 pub struct SchemaField {
     // Name of this (possibly nested) column
     name: String,
-    // String containing the name of a primitive type, a struct definition, an array definition or
-    // a map definition
     r#type: SchemaDataType,
     // Boolean denoting whether this field can be null
     nullable: bool,
@@ -39,23 +45,28 @@ pub struct SchemaField {
 }
 
 impl SchemaField {
+    /// The column name of the schema field.
     pub fn get_name(&self) -> &str {
         &self.name
     }
 
+    /// The data type of the schema field. SchemaDataType defines the possible values.
     pub fn get_type(&self) -> &SchemaDataType {
         &self.r#type
     }
 
+    /// Whether the column/field is nullable.
     pub fn is_nullable(&self) -> bool {
         self.nullable
     }
 
+    /// Additional metadata about the column/field.
     pub fn get_metadata(&self) -> &HashMap<String, String> {
         &self.metadata
     }
 }
 
+/// Schema definition for array type fields.
 #[derive(Serialize, Deserialize, Debug, Clone)]
 pub struct SchemaTypeArray {
     // type field is always the string "array", so we are ignoring it here
@@ -68,31 +79,34 @@ pub struct SchemaTypeArray {
 }
 
 impl SchemaTypeArray {
+    /// The data type of each element contained in the array.
     pub fn get_element_type(&self) -> &SchemaDataType {
         &self.elementType
     }
 
+    /// Whether the column/field is allowed to contain null elements.
     pub fn contains_null(&self) -> bool {
         self.containsNull
     }
 }
 
+/// Schema definition for map type fields.
 #[derive(Serialize, Deserialize, Debug, Clone)]
 pub struct SchemaTypeMap {
     r#type: String,
-    // The type of element used for the key of this map, represented as a string containing the
-    // name of a primitive type, a struct definition, an array definition or a map definition
     keyType: Box<SchemaDataType>,
-    // The type of element used for the key of this map, represented as a string containing the
-    // name of a primitive type, a struct definition, an array definition or a map definition
     valueType: Box<SchemaDataType>,
 }
 
 impl SchemaTypeMap {
+    /// The type of element used for the key of this map, represented as a string containing the
+    /// name of a primitive type, a struct definition, an array definition or a map definition
     pub fn get_key_type(&self) -> &SchemaDataType {
         &self.keyType
     }
 
+    /// The type of element contained in the value of this map, represented as a string containing the
+    /// name of a primitive type, a struct definition, an array definition or a map definition
     pub fn get_value_type(&self) -> &SchemaDataType {
         &self.valueType
     }
@@ -112,15 +126,22 @@ impl SchemaTypeMap {
  *   date: A calendar date, represented as a year-month-day triple without a timezone
  *   timestamp: Microsecond precision timestamp without a timezone
  */
+/// Enum with variants for each top level schema data type.
 #[derive(Serialize, Deserialize, Debug, Clone)]
 #[serde(untagged)]
 pub enum SchemaDataType {
+    /// Variant representing non-array, non-map, non-struct fields. Wrapped value will contain the
+    /// the string name of the primitive type.
     primitive(String),
+    /// Variant representing a struct.
     r#struct(SchemaTypeStruct),
+    /// Variant representing an array.
     array(SchemaTypeArray),
+    /// Variant representing a map.
     map(SchemaTypeMap),
 }
 
+/// Represents the schema of the delta table.
 #[derive(Serialize, Deserialize, Debug, Clone)]
 pub struct Schema {
     r#type: String,
@@ -128,6 +149,7 @@ pub struct Schema {
 }
 
 impl Schema {
+    /// Returns the list of fields that make up the schema definition of the table.
     pub fn get_fields(&self) -> &Vec<SchemaField> {
         &self.fields
     }
