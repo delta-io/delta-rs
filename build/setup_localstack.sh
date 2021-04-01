e39e64358207b41ddc7dc3fc0727202adc50e9b9
mosyp <mykhailo.osypov@gmail.com>
2021-04-01 13:10:02 +0300

    Use amazon/aws-cli for setup container, move s3 tests into separate job

--- build/setup_localstack.sh
+++ build/setup_localstack.sh
@@ -1,19 +1,20 @@
-#!/bin/sh
+#!/bin/bash
 
 export AWS_DEFAULT_REGION=us-east-2
 export AWS_ACCESS_KEY_ID=test
 export AWS_SECRET_ACCESS_KEY=test
 export ENDPOINT=http://localstack:4566
 
-timeout=120 #seconds
-while [ "$(curl -s $ENDPOINT/health | jq '.services.s3')" != "\"running\"" ]; do
-  if [ "$timeout" -lt "0" ]; then
-    echo 'S3 is offline for more than 2 minutes';
+retries=10 #seconds
+until aws s3api list-buckets --endpoint-url=$ENDPOINT > /dev/null 2>&1
+do
+  if [ "$retries" -lt "0" ]; then
+    echo 'S3 is still offline after 10 retries';
     exit 1;
   fi
   echo 'waiting on S3 to start...'
   sleep 5
-  timeout=$((timeout - 5))
+  retries=$((retries - 1))
 done
 
 echo 'S3 is running'
