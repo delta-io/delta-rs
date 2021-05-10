41afa27025a6a3f00e8c9163000b032e076b8286
fvaleye <florian.valeye@gmail.com>
2021-05-10 09:28:55 +0200

    Release Python bindings 0.4.8 (#241)

--- python/docs/source/conf.py
+++ python/docs/source/conf.py
@@ -50,6 +50,7 @@ nitpick_ignore = [
     ("py:class", "pyarrow.lib.Table"),
     ("py:class", "pyarrow.lib.DataType"),
     ("py:class", "pyarrow.lib.Field"),
+    ("py:class", "RawDeltaTable"),
 ]
 
 # Add any paths that contain templates here, relative to this directory.
