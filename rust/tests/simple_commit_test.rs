17274ac6f6ec06a472e2df05369739ff95d721a6
Christian <christianw@scribd.com>
2021-05-18 11:46:45 -0400

    Add tests and Err restructure for commit_version

--- rust/tests/simple_commit_test.rs
+++ rust/tests/simple_commit_test.rs
@@ -13,45 +13,121 @@ use std::collections::HashMap;
 
 use deltalake::{action, DeltaTransactionError};
 
-#[tokio::test]
-async fn test_two_commits_fs() {
-    prepare_fs();
-    test_two_commits("./tests/data/simple_commit")
-        .await
-        .unwrap();
-}
+mod simple_commit_s3 {
+    use super::*;
 
-#[cfg(all(feature = "s3", feature = "dynamodb"))]
-#[tokio::test]
-async fn test_two_commits_s3() {
-    let path = "s3://deltars/simple_commit_rw1";
-    s3_common::setup_dynamodb("concurrent_writes");
-    prepare_s3(path).await;
+    #[cfg(all(feature = "s3", feature = "dynamodb"))]
+    #[tokio::test]
+    async fn test_two_commits_s3() {
+        let path = "s3://deltars/simple_commit_rw1";
+        s3_common::setup_dynamodb("concurrent_writes");
+        prepare_s3(path).await;
 
-    test_two_commits(path).await.unwrap();
-}
-
-#[cfg(all(feature = "s3", not(feature = "dynamodb")))]
-#[tokio::test]
-async fn test_two_commits_s3_fails_with_no_lock() {
-    use deltalake::{StorageError, TransactionCommitAttemptError};
-
-    let path = "s3://deltars/simple_commit_rw2";
-    prepare_s3(path).await;
-
-    let result = test_two_commits(path).await;
-    if let Err(DeltaTransactionError::CommitRetriesExceeded { ref inner }) = result {
-        if let TransactionCommitAttemptError::Storage { source } = inner {
-            if let StorageError::S3Generic(err) = source {
-                assert_eq!(err, "dynamodb locking is not enabled");
-                return;
-            }
-        }
+        test_two_commits(path).await.unwrap();
     }
 
-    result.unwrap();
+    #[cfg(all(feature = "s3", not(feature = "dynamodb")))]
+    #[tokio::test]
+    async fn test_two_commits_s3_fails_with_no_lock() {
+        use deltalake::{StorageError, TransactionCommitAttemptError};
 
-    panic!("S3 commit without dynamodb locking is expected to fail")
+        let path = "s3://deltars/simple_commit_rw2";
+        prepare_s3(path).await;
+
+        let result = test_two_commits(path).await;
+        if let Err(DeltaTransactionError::TransactionCommitAttempt { ref inner }) = result {
+            if let TransactionCommitAttemptError::Storage { source } = inner {
+                if let StorageError::S3Generic(err) = source {
+                    assert_eq!(err, "dynamodb locking is not enabled");
+                    return;
+                }
+            }
+        }
+
+        result.unwrap();
+
+        panic!("S3 commit without dynamodb locking is expected to fail")
+    }
+}
+
+mod simple_commit_fs {
+    // Tests are run serially to allow usage of the same local fs directory.
+    use serial_test::serial;
+
+    use super::*;
+
+    #[tokio::test]
+    #[serial]
+    async fn test_two_commits_fs() {
+        prepare_fs();
+        test_two_commits("./tests/data/simple_commit")
+            .await
+            .unwrap();
+    }
+
+    #[tokio::test]
+    #[serial]
+    async fn test_commit_version_succeeds_if_version_does_not_exist() {
+        prepare_fs();
+
+        let table_path = "./tests/data/simple_commit";
+        let mut table = deltalake::open_table(table_path).await.unwrap();
+
+        assert_eq!(0, table.version);
+        assert_eq!(0, table.get_files().len());
+
+        let tx1_actions = tx1_actions();
+
+        let mut tx1 = table.create_transaction(None);
+        let result = tx1
+            .commit_version(1, tx1_actions.as_slice(), None)
+            .await
+            .unwrap();
+
+        assert_eq!((), result);
+        assert_eq!(1, table.version);
+        assert_eq!(2, table.get_files().len());
+    }
+
+    #[tokio::test]
+    #[serial]
+    async fn test_commit_version_fails_if_version_exists() {
+        prepare_fs();
+
+        let table_path = "./tests/data/simple_commit";
+        let mut table = deltalake::open_table(table_path).await.unwrap();
+
+        assert_eq!(0, table.version);
+        assert_eq!(0, table.get_files().len());
+
+        let tx1_actions = tx1_actions();
+
+        let mut tx1 = table.create_transaction(None);
+        let _ = tx1
+            .commit_version(1, tx1_actions.as_slice(), None)
+            .await
+            .unwrap();
+
+        let tx2_actions = tx2_actions();
+
+        let mut tx2 = table.create_transaction(None);
+
+        // we already committed version 1 - this should fail and return error for caller to handle.
+        let result = tx2.commit_version(1, tx2_actions.as_slice(), None).await;
+
+        match result {
+            Err(deltalake::DeltaTransactionError::VersionAlreadyExists { .. }) => {
+                assert!(true, "Delta version already exists.");
+            }
+            _ => {
+                assert!(false, "Delta version should already exist.");
+            }
+        }
+
+        assert!(result.is_err());
+        assert_eq!(1, table.version);
+        assert_eq!(2, table.get_files().len());
+    }
 }
 
 async fn test_two_commits(table_path: &str) -> Result<(), DeltaTransactionError> {
@@ -60,7 +136,28 @@ async fn test_two_commits(table_path: &str) -> Result<(), DeltaTransactionError>
     assert_eq!(0, table.version);
     assert_eq!(0, table.get_files().len());
 
-    let tx1_actions = vec![
+    let tx1_actions = tx1_actions();
+
+    let mut tx1 = table.create_transaction(None);
+    let version = tx1.commit_with(tx1_actions.as_slice(), None).await?;
+
+    assert_eq!(1, version);
+    assert_eq!(version, table.version);
+    assert_eq!(2, table.get_files().len());
+
+    let tx2_actions = tx2_actions();
+
+    let mut tx2 = table.create_transaction(None);
+    let version = tx2.commit_with(tx2_actions.as_slice(), None).await.unwrap();
+
+    assert_eq!(2, version);
+    assert_eq!(version, table.version);
+    assert_eq!(4, table.get_files().len());
+    Ok(())
+}
+
+fn tx1_actions() -> Vec<action::Action> {
+    vec![
         action::Action::add(action::Add {
             path: String::from(
                 "part-00000-b44fcdb0-8b06-4f3a-8606-f8311a96f6dc-c000.snappy.parquet",
@@ -87,16 +184,11 @@ async fn test_two_commits(table_path: &str) -> Result<(), DeltaTransactionError>
             stats_parsed: None,
             tags: None,
         }),
-    ];
+    ]
+}
 
-    let mut tx1 = table.create_transaction(None);
-    let version = tx1.commit_with(tx1_actions.as_slice(), None).await?;
-
-    assert_eq!(1, version);
-    assert_eq!(version, table.version);
-    assert_eq!(2, table.get_files().len());
-
-    let tx2_actions = vec![
+fn tx2_actions() -> Vec<action::Action> {
+    vec![
         action::Action::add(action::Add {
             path: String::from(
                 "part-00000-512e1537-8aaa-4193-b8b4-bef3de0de409-c000.snappy.parquet",
@@ -123,15 +215,7 @@ async fn test_two_commits(table_path: &str) -> Result<(), DeltaTransactionError>
             stats_parsed: None,
             tags: None,
         }),
-    ];
-
-    let mut tx2 = table.create_transaction(None);
-    let version = tx2.commit_with(tx2_actions.as_slice(), None).await.unwrap();
-
-    assert_eq!(2, version);
-    assert_eq!(version, table.version);
-    assert_eq!(4, table.get_files().len());
-    Ok(())
+    ]
 }
 
 fn prepare_fs() {
