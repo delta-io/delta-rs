0c3853ea13c4105ff2902486bc34e6e5feccf317
fvaleye <florian.valeye@gmail.com>
2021-05-24 19:51:57 +0200

    Improve dev experience with Python binding (#263)
    * Add get_file_paths_by_partitions in Python bindings
    
    * Add makefile, mypy, isort

--- python/deltalake/__init__.py
+++ python/deltalake/__init__.py
@@ -1,3 +1,3 @@
 from .deltalake import RawDeltaTable, RawDeltaTableMetaData, rust_core_version
+from .schema import DataType, Field, Schema
 from .table import DeltaTable, Metadata
-from .schema import Schema, Field, DataType
--- python/docs/source/conf.py
+++ python/docs/source/conf.py
@@ -12,6 +12,7 @@
 #
 import os
 import sys
+
 import toml
 
 sys.path.insert(0, os.path.abspath("../deltalake/"))
