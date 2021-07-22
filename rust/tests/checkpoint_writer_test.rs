8321c67350d2b4936a0688d60ed8422e019066e5
mosyp <mykhailo.osypov@gmail.com>
2021-07-22 13:23:49 +0300

    Add test case

--- rust/tests/checkpoint_writer_test.rs
+++ rust/tests/checkpoint_writer_test.rs
@@ -18,7 +18,7 @@ async fn write_simple_checkpoint() {
     cleanup_checkpoint_files(log_path.as_path());
 
     // Load the delta table at version 5
-    let table = deltalake::open_table_with_version(table_location, 5)
+    let mut table = deltalake::open_table_with_version(table_location, 5)
         .await
         .unwrap();
 
@@ -39,27 +39,48 @@ async fn write_simple_checkpoint() {
     // Error("EOF while parsing a value", line: 1, column: 0)'
     std::thread::sleep(std::time::Duration::from_secs(1));
 
-    // _last_checkpoint should exist
-    let last_checkpoint_path = log_path.join("_last_checkpoint");
-    assert!(last_checkpoint_path.as_path().exists());
-
-    // _last_checkpoint should point to the correct version
-    let last_checkpoint_content = fs::read_to_string(last_checkpoint_path.as_path()).unwrap();
-    let last_checkpoint_content: serde_json::Value =
-        serde_json::from_str(last_checkpoint_content.trim()).unwrap();
-
-    let version = last_checkpoint_content
-        .get("version")
-        .unwrap()
-        .as_i64()
-        .unwrap();
+    // _last_checkpoint should exist and point to the correct version
+    let version = get_last_checkpoint_version(&log_path);
     assert_eq!(5, version);
+
+    // Setting table version to 10 for another checkpoint
+    table.load_version(10).await.unwrap();
+    checkpoint_writer
+        .create_checkpoint_from_state(table.version, table.get_state())
+        .await
+        .unwrap();
+
+    // checkpoint should exist
+    let checkpoint_path = log_path.join("00000000000000000010.checkpoint.parquet");
+    assert!(checkpoint_path.as_path().exists());
+
+    // see above
+    std::thread::sleep(std::time::Duration::from_secs(1));
+
+    // _last_checkpoint should exist and point to the correct version
+    let version = get_last_checkpoint_version(&log_path);
+    assert_eq!(10, version);
 
     // delta table should load just fine with the checkpoint in place
     let table_result = deltalake::open_table(table_location).await.unwrap();
     let table = table_result;
     let files = table.get_files();
     assert_eq!(11, files.len());
+}
+
+fn get_last_checkpoint_version(log_path: &PathBuf) -> i64 {
+    let last_checkpoint_path = log_path.join("_last_checkpoint");
+    assert!(last_checkpoint_path.as_path().exists());
+
+    let last_checkpoint_content = fs::read_to_string(last_checkpoint_path.as_path()).unwrap();
+    let last_checkpoint_content: serde_json::Value =
+        serde_json::from_str(last_checkpoint_content.trim()).unwrap();
+
+    last_checkpoint_content
+        .get("version")
+        .unwrap()
+        .as_i64()
+        .unwrap()
 }
 
 fn cleanup_checkpoint_files(log_path: &Path) {
