773864d69568466047d555b013d8e0c46b4826c4
Mykhailo Osypov <mykhailo.osypov@gmail.com>
2021-10-06 19:26:50 +0300

    Fix broken tombstones metadata when extended_file_metadata is different between tomstones in state (#450)
    * Fix broken tombstones metadata when extended_file_metadata is different between tomstones in state
    
    * clippy

--- rust/tests/fs_common/mod.rs
+++ rust/tests/fs_common/mod.rs
@@ -1,7 +1,10 @@
-use deltalake::action::Protocol;
+use deltalake::action::{Action, Protocol};
 use deltalake::{storage, DeltaTable, DeltaTableMetaData, Schema};
+use parquet::file::reader::{FileReader, SerializedFileReader};
+use parquet::schema::types::Type;
 use std::collections::HashMap;
 use std::fs;
+use std::fs::File;
 use std::path::Path;
 
 pub fn cleanup_dir_except<P: AsRef<Path>>(path: P, ignore_files: Vec<String>) {
@@ -31,4 +34,16 @@ pub async fn create_test_table(
     };
     table.create(md, protocol, None).await.unwrap();
     table
+}
+
+pub async fn read_checkpoint(path: &str) -> (Type, Vec<Action>) {
+    let file = File::open(path).unwrap();
+    let reader = SerializedFileReader::new(file).unwrap();
+    let schema = reader.metadata().file_metadata().schema();
+    let mut row_iter = reader.get_row_iter(None).unwrap();
+    let mut actions = Vec::new();
+    while let Some(record) = row_iter.next() {
+        actions.push(Action::from_parquet_record(schema, &record).unwrap())
+    }
+    (schema.clone(), actions)
 }
