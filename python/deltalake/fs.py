fa7386b1029cb09e271521dda19a6127da667eb1
Robert Pack <robstar.pack@gmail.com>
2021-10-19 11:59:22 +0200

    make mypy happy

--- python/deltalake/fs.py
+++ python/deltalake/fs.py
@@ -1,4 +1,4 @@
-from typing import List
+from typing import Dict, List
 
 import pyarrow as pa
 import pyarrow.fs as pa_fs
@@ -8,16 +8,16 @@ from .deltalake import DeltaStorageFsBackend
 
 class DeltaStorageHandler(pa_fs.FileSystemHandler):
 
-    def __init__(self, table_uri: str):
+    def __init__(self, table_uri: str) -> None:
         self._storage = DeltaStorageFsBackend(table_uri)
 
-    def __eq__(self, other):
+    def __eq__(self, other) -> bool:
         return NotImplemented
 
-    def __ne__(self, other):
+    def __ne__(self, other) -> bool:
         return NotImplemented
 
-    def get_type_name(self):
+    def get_type_name(self) -> str:
         return NotImplemented
 
     def normalize_path(self, path: str) -> str:
@@ -33,37 +33,37 @@ class DeltaStorageHandler(pa_fs.FileSystemHandler):
     def get_file_info_selector(self, selector):
         raise NotImplementedError
 
-    def create_dir(self, path, recursive):
+    def create_dir(self, path: str, *, recursive: bool = True) -> None:
         raise NotImplementedError
 
-    def delete_dir(self, path):
+    def delete_dir(self, path: str) -> None:
         raise NotImplementedError
 
-    def delete_dir_contents(self, path):
+    def delete_dir_contents(self, path: str) -> None:
         raise NotImplementedError
 
-    def delete_root_dir_contents(self):
+    def delete_root_dir_contents(self) -> None:
         raise NotImplementedError
 
-    def delete_file(self, path):
+    def delete_file(self, path: str) -> None:
         raise NotImplementedError
 
-    def move(self, src, dest):
+    def move(self, src: str, dest: str) -> None:
         raise NotImplementedError
 
-    def copy_file(self, src, dest):
+    def copy_file(self, src: str, dest: str) -> None:
         raise NotImplementedError
 
-    def open_input_stream(self, path):
+    def open_input_stream(self, path) -> pa.NativeFile:
         raw = self._storage.get_obj(path)
         return pa.BufferReader(pa.py_buffer(raw))
 
-    def open_input_file(self, path):
+    def open_input_file(self, path) -> pa.NativeFile:
         raw = self._storage.get_obj(path)
         return pa.BufferReader(pa.py_buffer(raw))
 
-    def open_output_stream(self, path, metadata):
+    def open_output_stream(self, path: str, metadata: Dict = None) -> pa.NativeFile:
         raise NotImplementedError
 
-    def open_append_stream(self, path, metadata):
+    def open_append_stream(self, path: str, metadata: Dict = None) -> pa.NativeFile:
         raise NotImplementedError
