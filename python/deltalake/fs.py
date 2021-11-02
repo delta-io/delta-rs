21b5ae1e6aa3a5b6c067d63fb3c3e2ed90b7f0a4
Robert Pack <42610831+roeap@users.noreply.github.com>
2021-11-02 04:28:26 +0100

    Fix python build (#487)
    * fix some docstrings
    
    * see what happens if we add more docstrings
    
    * add socstrings to not implemented methods
    
    * fix comments

--- python/deltalake/fs.py
+++ python/deltalake/fs.py
@@ -21,13 +21,18 @@ class DeltaStorageHandler(FileSystemHandler):
         return NotImplemented
 
     def get_type_name(self) -> str:
+        """
+        The filesystem’s type name.
+
+        :return: The filesystem’s type name.
+        """
         return NotImplemented
 
     def normalize_path(self, path: str) -> str:
         """
         Normalize filesystem path.
 
-        :param path:the path to normalize
+        :param path: the path to normalize
         :return: the normalized path
         """
         return self._storage.normalize_path(path)
@@ -46,34 +51,89 @@ class DeltaStorageHandler(FileSystemHandler):
         return infos
 
     def get_file_info_selector(self, selector: FileSelector) -> List[FileInfo]:
+        """
+        Get info for the files defined by FileSelector.
+
+        :param selector: FileSelector object
+        :return: list of file info objects
+        """
         raise NotImplementedError
 
     def create_dir(self, path: str, *, recursive: bool = True) -> None:
+        """
+        Create a directory and subdirectories.
+
+        This function succeeds if the directory already exists.
+        
+        :param path: The path of the new directory.
+        :param recursive: Create nested directories as well.
+        """
         raise NotImplementedError
 
     def delete_dir(self, path: str) -> None:
+        """
+        Delete a directory and its contents, recursively.
+
+        :param path: The path of the directory to be deleted.
+        """
         raise NotImplementedError
 
     def delete_dir_contents(self, path: str) -> None:
+        """
+        Delete a directory’s contents, recursively.
+
+        Like delete_dir, but doesn’t delete the directory itself.
+
+        :param path: The path of the directory to be deleted.
+        """
         raise NotImplementedError
 
     def delete_root_dir_contents(self) -> None:
+        """
+        Delete a directory’s contents, recursively.
+
+        Like delete_dir_contents, but for the root directory (path is empty or “/”)
+        """
         raise NotImplementedError
 
     def delete_file(self, path: str) -> None:
+        """
+        Delete a file.
+
+        :param path: The path of the file to be deleted.
+        """
         raise NotImplementedError
 
     def move(self, src: str, dest: str) -> None:
+        """
+        Move / rename a file or directory.
+
+        If the destination exists: - if it is a non-empty directory, an error is returned - otherwise,
+        if it has the same type as the source, it is replaced - otherwise,
+        behavior is unspecified (implementation-dependent).
+
+        :param src: The path of the file or the directory to be moved.
+        :param dest: The destination path where the file or directory is moved to.
+        """
         raise NotImplementedError
 
     def copy_file(self, src: str, dest: str) -> None:
+        """
+        Copy a file.
+
+        If the destination exists and is a directory, an error is returned.
+        Otherwise, it is replaced.
+
+        :param src: The path of the file to be copied from.
+        :param dest: The destination path where the file is copied to.
+        """
         raise NotImplementedError
 
     def open_input_stream(self, path: str) -> pa.NativeFile:
         """
         Open an input stream for sequential reading.
 
-        :param source The source to open for reading.
+        :param source: The source to open for reading.
         :return:  NativeFile
         """
         raw = self._storage.get_obj(path)
@@ -83,7 +143,7 @@ class DeltaStorageHandler(FileSystemHandler):
         """
         Open an input file for random access reading.
 
-        :param source The source to open for reading.
+        :param source: The source to open for reading.
         :return:  NativeFile
         """
         raw = self._storage.get_obj(path)
@@ -92,9 +152,27 @@ class DeltaStorageHandler(FileSystemHandler):
     def open_output_stream(
         self, path: str, metadata: Optional[Dict[str, Any]] = None
     ) -> pa.NativeFile:
+        """
+        Open an output stream for sequential writing.
+
+        If the target already exists, existing data is truncated.
+
+        :param path: The source to open for writing.
+        :param metadata: If not None, a mapping of string keys to string values.
+        :return:  NativeFile
+        """
         raise NotImplementedError
 
     def open_append_stream(
         self, path: str, metadata: Optional[Dict[str, Any]] = None
     ) -> pa.NativeFile:
+        """
+        DEPRECATED: Open an output stream for appending.
+
+        If the target doesn’t exist, a new empty file is created.
+
+        :param path: The source to open for writing.
+        :param metadata: If not None, a mapping of string keys to string values.
+        :return:  NativeFile
+        """
         raise NotImplementedError
