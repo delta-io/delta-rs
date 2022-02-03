96d2a229235be53d708253db81b6ed097a488937
Thomas Vollmer <34384788+thovoll@users.noreply.github.com>
2022-02-03 05:39:54 -0800

    Automatically create and delete test file system.

--- rust/tests/adls_gen2_backend_test.rs
+++ rust/tests/adls_gen2_backend_test.rs
@@ -1,8 +1,11 @@
 #[cfg(feature = "azure")]
 mod adls_gen2_backend {
+    use azure_storage::storage_shared_key_credential::StorageSharedKeyCredential;
+    use azure_storage_datalake::clients::DataLakeClient;
     use chrono::Utc;
     use deltalake::StorageError;
     use serial_test::serial;
+    use std::env;
 
     /*
      * An Azure Data Lake Gen2 Storage Account is required to run these tests and must be provided by
@@ -16,23 +19,47 @@ mod adls_gen2_backend {
     #[ignore]
     #[tokio::test]
     #[serial]
-    async fn test_adls_gen2_backend_put_and_delete_obj_with_dir() {
-        let account = std::env::var("AZURE_STORAGE_ACCOUNT_NAME").unwrap();
-        let base_path = &format!("adls2://{}/fs-put-and-delete-obj-with-dir/", account);
-        let backend = deltalake::get_backend_for_uri(base_path).unwrap();
+    async fn test_put_and_delete_obj_with_dir() {
+        // Arrange
+        let storage_account_name = env::var("AZURE_STORAGE_ACCOUNT_NAME").unwrap();
+        let storage_account_key = env::var("AZURE_STORAGE_ACCOUNT_KEY").unwrap();
+
+        let data_lake_client = DataLakeClient::new(
+            StorageSharedKeyCredential::new(
+                storage_account_name.to_owned(),
+                storage_account_key.to_owned(),
+            ),
+            None,
+        );
+
+        // Create a new file system for test isolation
+        let file_system_name = format!("test-adlsgen2-backend-{}", Utc::now().timestamp());
+        let file_system_client =
+            data_lake_client.into_file_system_client(file_system_name.to_owned());
+        file_system_client.create().into_future().await.unwrap();
+
+        let table_uri = &format!("adls2://{}/{}/", storage_account_name, file_system_name);
+        let backend = deltalake::get_backend_for_uri(table_uri).unwrap();
 
         let ts = Utc::now().timestamp();
-        let file_path = &format!("{}dir1/file-{}.txt", base_path, ts);
+        let file_path = &format!("{}put-and-delete-obj-with-dir/file-{}.txt", table_uri, ts);
 
+        // Act 1
         backend.put_obj(file_path, &[12, 13, 14]).await.unwrap();
 
+        // Assert 1
         let file_meta_data = backend.head_obj(file_path).await.unwrap();
         assert_eq!(file_meta_data.path, *file_path);
 
+        // Act 2
         // Note: dir1 itself does not get deleted here, just the file
         backend.delete_obj(file_path).await.unwrap();
 
+        // Assert 2
         let head_err = backend.head_obj(file_path).await.err().unwrap();
         assert!(matches!(head_err, StorageError::NotFound));
+
+        // Cleanup
+        file_system_client.delete().into_future().await.unwrap();
     }
 }
