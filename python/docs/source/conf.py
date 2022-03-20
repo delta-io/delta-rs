a4ebf7f5eb4f3713ed413a9769b23d8e97be333c
Will Jones <willjones127@gmail.com>
2022-03-20 11:18:35 -0700

    cleanup

--- python/docs/source/conf.py
+++ python/docs/source/conf.py
@@ -64,7 +64,6 @@ nitpick_ignore = [
     ("py:class", "pyarrow._fs.FileSystemHandler"),
     ("py:class", "RawDeltaTable"),
     ("py:class", "pandas.DataFrame"),
-    ("py:class", "typing_extensions.Literal"),
 ]
 
 # Add any paths that contain templates here, relative to this directory.
