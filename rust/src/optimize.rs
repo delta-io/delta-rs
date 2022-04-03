814647a5290ee04429c9284eec01352a541cda8a
David Blajda <db@davidblajda.com>
2022-04-03 19:53:12 -0400

    :WIP: Optimize for tables with partitions

--- rust/src/optimize.rs
+++ rust/src/optimize.rs
@@ -1,6 +1,7 @@
 //! TODO: Optimize
 
 use std::collections::HashMap;
+use std::collections::HashSet;
 use std::hash::{Hash, Hasher};
 use std::sync::Arc;
 use std::time::{SystemTime, UNIX_EPOCH};
@@ -15,35 +16,58 @@ use parquet::file::writer::InMemoryWriteableCursor;
 use crate::action::{self, Action};
 use crate::{
     generate_parquet_filename, DeltaDataTypeLong, DeltaTable, DeltaTableError, DeltaTablePartition,
-    PartitionFilter,
+    PartitionFilter, SchemaTypeStruct,
 };
 
-/*
-{"numFilesAdded": 13, "numFilesRemoved": 3021, "filesAdded": {"min": 31390063,
-"max": 254248103, "avg": 216181665.23076922, "totalFiles": 13, "totalSize":
-2810361648}, "filesRemoved": {"min": 8270, "max": 2516341, "avg":
-1001047.8202581926, "totalFiles": 3021, "totalSize": 3024165465},
-"partitionsOptimized": 2, "zOrderStats": null, "numBatches": 1,
-"totalConsideredFiles": 3021, "totalFilesSkipped": 0, "preserveInsertionOrder":
-true}
-*/
 #[derive(Default, Debug)]
 ///Metrics from Optimize
 pub struct Metrics {
-    ///TODO
+    ///Number of optimized files added
     pub num_files_added: u64,
-    ///TODO
+    ///Number of unoptimized files removed
     pub num_files_removed: u64,
-    ///TODO
+    ///Detailed metrics for the add operation
+    pub files_added: MetricDetails,
+    ///Detailed metrics for the remove operation
+    pub files_removed: MetricDetails,
+    ///Number of partitions that had at least one file optimized
     pub partitions_optimized: u64,
     ///TODO
     pub num_batches: u64,
-    ///TODO
-    pub total_considered_files: u64,
-    ///TODO
-    pub total_files_skipped: u64,
+    ///How many files were considered during optimization. Not every file considered is optimized
+    pub total_considered_files: usize,
+    ///How many files were considered for optimization but were skipped
+    pub total_files_skipped: usize,
     ///TODO
     pub preserve_insertion_order: bool,
+}
+
+#[derive(Debug)]
+///Statistics on files for a particular operation
+/// Operation can be remove or add
+pub struct MetricDetails {
+    ///Maximum file size of a operation
+    pub min: usize,
+    ///Minimum file size of a operation
+    pub max: usize,
+    ///Average file size of a operation
+    pub avg: f64,
+    ///Number of files encountered during operation
+    pub total_files: usize,
+    ///Sum of file sizes of a operation
+    pub total_size: usize,
+}
+
+impl Default for MetricDetails {
+    fn default() -> Self {
+        MetricDetails {
+            min: usize::MAX,
+            max: 0,
+            avg: 0.0,
+            total_files: 0,
+            total_size: 0,
+        }
+    }
 }
 
 ///TODO: Optimize
@@ -54,7 +78,7 @@ pub struct Optimize<'a> {
 
 impl<'a> Optimize<'a> {
     ///Only optimize files that belong to the specified filter
-    pub fn _where(&mut self, filters: &'a [PartitionFilter<'a, &'a str>]) -> &Self {
+    pub fn _where(mut self, filters: &'a [PartitionFilter<'a, &'a str>]) -> Self {
         self.filters = Some(filters);
         return self;
     }
@@ -67,7 +91,7 @@ impl<'a> Optimize<'a> {
         } else {
             create_merge_plan(table, self.filters.as_ref().unwrap())?
         };
-        print!("Merge Plan: \n {:?}", plan);
+        println!("Merge Plan: \n {:?}", plan);
         let metrics = plan.execute(table).await?;
         return Ok(metrics);
     }
@@ -80,7 +104,7 @@ impl<'a> From<&DeltaTablePartition<'a>> for Partition {
     fn from(p: &DeltaTablePartition<'a>) -> Self {
         Partition {
             key: p.key.to_owned(),
-            value: p.key.to_owned(),
+            value: p.value.to_owned(),
         }
     }
 }
@@ -244,17 +268,30 @@ impl MergePlan {
         }))
     }
 
-    pub async fn execute(self, table: &mut DeltaTable) -> Result<Metrics, DeltaTableError> {
+    pub async fn execute(mut self, table: &mut DeltaTable) -> Result<Metrics, DeltaTableError> {
         //Read files into memory and write into memory. Once a file is complete write to underlying storage.
-        let schema = &table.get_metadata()?.clone().schema;
+        let schema = table.get_metadata()?.clone().schema.clone();
+
+        let columns = &table.state.current_metadata().unwrap().partition_columns;
+        let partition_column_set: HashSet<String> = columns.into_iter().map(|s| s.to_owned()).collect();
+
+        //Remove partitions from the schema since they don't need to written to the parquet file
+        let fields = schema
+            .get_fields()
+            .iter()
+            .filter(|field| !partition_column_set.contains(field.get_name()))
+            .map(|e| e.to_owned())
+            .collect();
+        let schema = SchemaTypeStruct::new(fields);
+
         let arrow_schema =
             <arrow::datatypes::Schema as TryFrom<&crate::Schema>>::try_from(&schema).unwrap();
         let schema = Arc::new(arrow_schema);
         let mut actions = vec![];
 
         for (_partitions, merges) in self.operations.iter() {
-            println!("{:?}", merges);
-            println!("{:?}", _partitions);
+            debug!("{:?}", merges);
+            debug!("{:?}", _partitions);
             for merge in merges {
                 //Open some buffer
                 //Extra: track statistics for the file
@@ -267,19 +304,36 @@ impl MergePlan {
                     schema.clone(),
                     Some(writer_properties),
                 )?;
+                println!("Writer Schema");
+                println!("{:?}", schema);
+
                 for path in merge {
                     //load the file into memory and append it to the buffer
 
                     let parquet_uri = table.storage.join_path(&table.table_uri, &path);
                     println!("Open {:?}", parquet_uri);
                     let data = table.storage.get_obj(&parquet_uri).await?;
-                    let data = SliceableCursor::new(data);
                     let size = data.len();
+                    let data = SliceableCursor::new(data);
                     let reader = SerializedFileReader::new(data)?;
 
                     //TODO: Can this handle schema changes?
                     let mut arrow_reader = ParquetFileArrowReader::new(Arc::new(reader));
-                    let batch_reader = arrow_reader.get_record_reader(2048)?;
+
+                    let mut reader_indicies = vec![];
+                    let mut i: usize = 0;
+                    for f in arrow_reader.get_schema()?.fields() {
+                        if !partition_column_set.contains(f.name()) {
+                            reader_indicies.push(i);
+                        }
+                        i += 1;
+                    }
+
+                    //arrow_reader.get_schema_by_columns(, false);
+                    //let reader_schema = arrow_reader.get_schema_by_columns(reader_indicies, false);
+                    println!("Reader Schema");
+                    println!("{:?}", arrow_reader.get_schema());
+                    let batch_reader = arrow_reader.get_record_reader_by_columns(reader_indicies, 2048)?;
                     for batch in batch_reader {
                         let batch = batch?;
                         _writer.write(&batch)?;
@@ -288,10 +342,19 @@ impl MergePlan {
                         &path,
                         self.to_internal_partition_format(_partitions),
                         size.try_into().unwrap(),
-                    )?)
+                    )?);
+
+                    self.metrics.num_files_removed += 1;
+                    self.metrics.files_removed.total_files += 1;
+                    self.metrics.files_removed.total_size += size;
+                    self.metrics.files_removed.max =
+                        std::cmp::max(self.metrics.files_removed.max, size);
+                    self.metrics.files_removed.min =
+                        std::cmp::min(self.metrics.files_removed.min, size);
                 }
                 //Save the file to storage and create corresponding add and delete actions. Do not commit yet.
                 _writer.close()?;
+                let size = writeable_cursor.data().len();
                 actions.push(
                     self.create_add(
                         table,
@@ -300,7 +363,15 @@ impl MergePlan {
                     )
                     .await?,
                 );
+
+                self.metrics.num_files_added += 1;
+                self.metrics.files_added.total_files += 1;
+                self.metrics.files_added.total_size += size;
+                self.metrics.files_added.max = std::cmp::max(self.metrics.files_added.max, size);
+                self.metrics.files_added.min = std::cmp::min(self.metrics.files_added.min, size);
             }
+            //Currently a table without any partitions has a count of one partition. Check if that is acceptable
+            self.metrics.partitions_optimized += 1;
         }
 
         //try to commit actions to the delta log.
@@ -308,6 +379,12 @@ impl MergePlan {
         let mut dtx = table.create_transaction(None);
         dtx.add_actions(actions);
         dtx.commit(None).await?;
+
+        self.metrics.files_added.avg = (self.metrics.files_added.total_size as f64)
+            / (self.metrics.files_added.total_files as f64);
+        self.metrics.files_removed.avg = (self.metrics.files_removed.total_size as f64)
+            / (self.metrics.files_removed.total_files as f64);
+        self.metrics.num_batches = 1;
 
         Ok(self.metrics)
     }
@@ -333,7 +410,7 @@ fn create_merge_plan<'a>(
 
     //Naively try to fit as many files as possible
     let mut operations: HashMap<Partitions, Merge> = HashMap::new();
-    let metrics = Metrics::default();
+    let mut metrics = Metrics::default();
     //TODO: Get this from table config
     let max_size: i64 = 1024 * 1024 * 200;
 
@@ -344,6 +421,7 @@ fn create_merge_plan<'a>(
 
         let partition = candidate.0;
         let files = candidate.1;
+        metrics.total_considered_files = files.len();
 
         for f in files {
             if f.size < max_size {
@@ -355,16 +433,22 @@ fn create_merge_plan<'a>(
                     //create a new bin. Discard old bin if it contains only one member
                     if current.len() > 1 {
                         opt.push(current);
+                    } else {
+                        metrics.total_files_skipped += 1;
                     }
                     current = Vec::new();
                     current_size = 0;
                     current.push(f.path.clone());
                 }
+            } else {
+                metrics.total_files_skipped += 1;
             }
         }
 
         if current.len() > 1 {
             opt.push(current);
+        } else {
+            metrics.total_files_skipped += 1;
         }
 
         operations.insert(Partitions::from(&partition), opt);
--- rust/tests/optimize_test.rs
+++ rust/tests/optimize_test.rs
@@ -2,17 +2,17 @@
 mod optiize {
     extern crate deltalake;
 
-    use arrow::array::Int64Array;
+    use arrow::array::{Int64Array, Int32Array};
     use datafusion::prelude::ExecutionContext;
-    use deltalake::{open_table, optimize::Optimize, DeltaTable, DeltaTableConfig};
+    use deltalake::{open_table, optimize::Optimize, PartitionFilter};
     use fs_extra::dir;
-    use std::{env, error::Error, matches, process::Command, sync::Arc};
+    use std::{error::Error, sync::Arc};
 
     #[tokio::test]
     async fn test_optimize_no_partitions() -> Result<(), Box<dyn Error>> {
         let tmp_dir = tempdir::TempDir::new("optimize_test")?;
         let path = tmp_dir.path();
-        let mut opt = dir::CopyOptions::new();
+        let opt = dir::CopyOptions::new();
         dir::copy("./tests/data/simple_table/", path, &opt)?;
         let p = path.join("simple_table").to_owned();
 
@@ -42,6 +42,48 @@ mod optiize {
         assert_eq!(
             batch.column(0).as_ref(),
             Arc::new(Int64Array::from(vec![5, 7, 9])).as_ref(),
+        );
+
+        Ok(())
+    }
+
+    #[tokio::test]
+    async fn test_optimize_with_partitions() -> Result<(), Box<dyn Error>> {
+        let tmp_dir = tempdir::TempDir::new("optimize_test")?;
+        let path = tmp_dir.path();
+        let opt = dir::CopyOptions::new();
+        dir::copy("./tests/data/optimize_partitions/", path, &opt)?;
+        let p = path.join("optimize_partitions").to_owned();
+
+        let mut dt = open_table(p.to_str().unwrap()).await?;
+        let version = dt.version;
+        let mut filter = vec![];
+        filter.push(PartitionFilter::try_from(("event_year", "=", "2022"))?);
+        filter.push(PartitionFilter::try_from(("event_month", "=", "04"))?);
+
+        let optimize = Optimize::default()._where(&filter);
+        let metrics = optimize.execute(&mut dt).await?;
+
+        println!("{:?}", metrics);
+        //Validate the data matches
+        assert_eq!(version + 1, dt.version);
+
+        let mut ctx = ExecutionContext::new();
+        let table = deltalake::open_table(p.to_str().unwrap()).await.unwrap();
+        ctx.register_table("demo", Arc::new(table))?;
+
+        let batches = ctx
+            .sql("SELECT id FROM demo ORDER BY id ASC")
+            .await?
+            .collect()
+            .await?;
+
+        assert_eq!(batches.len(), 1);
+        let batch = &batches[0];
+
+        assert_eq!(
+            batch.column(0).as_ref(),
+            Arc::new(Int32Array::from(vec![1, 2, 3, 4, 5, 6, 7, 8])).as_ref(),
         );
 
         Ok(())
