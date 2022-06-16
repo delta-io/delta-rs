9a4d0816a4e89305d494a1897731bc98b2c2ef18
Robert Pack <robstar.pack@gmail.com>
2022-06-16 13:59:48 +0200

    fix list operations in azure

--- rust/src/storage/azure/mod.rs
+++ rust/src/storage/azure/mod.rs
@@ -358,7 +358,11 @@ impl StorageBackend for AdlsGen2Backend {
         Ok(self
             .file_system_client
             .list_paths()
-            .directory(path)
+            // TODO this assumes we are always only interested in listing contents in one directory.
+            // to make list requests cheaper. As far as I can tell this should work in this case,
+            // although this behavior might be different in other object store implementations.
+            .recursive(false)
+            .directory(obj.path)
             .into_stream()
             .flat_map(|it| match it {
                 Ok(paths) => Either::Left(stream::iter(paths.into_iter().map(|p| {
