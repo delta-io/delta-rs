eba1a1d0d9dd25a7db49776cd88d92d3966f961f
Thomas Newton <thomas.w.newton@gmail.com>
2022-06-24 00:17:11 +0100

    Unittest covering more complex types

--- rust/tests/read_delta_test.rs
+++ rust/tests/read_delta_test.rs
@@ -472,6 +472,77 @@ async fn vacuum_delta_8_0_table() {
     );
 }
 
+
+#[tokio::test]
+async fn read_delta_1_2_1_struct_stats_table_without_version() {
+    let table = deltalake::open_table("./tests/data/delta-1.2.1-only-struct-stats")
+        .await
+        .unwrap();
+    assert_eq!(table.get_stats().count(), 12);
+
+    fn get_stats_for_file(
+        table: &deltalake::DeltaTable,
+        file_name: std::string::String,
+    ) -> deltalake::action::Stats {
+        table
+            .get_file_uris()
+            .zip(table.get_stats())
+            .filter_map(|(file_uri, file_stats)| {
+                println!("{file_uri}");
+                if file_uri.ends_with(&file_name) {
+                    return file_stats.unwrap();
+                }
+                return None;
+            })
+            .next()
+            .unwrap()
+    }
+
+    let first_file_stats = get_stats_for_file(
+        &table,
+        std::string::String::from(
+            "part-00000-b5f1c7b4-7d81-4461-ac30-4446d48a53a4-c000.snappy.parquet",
+        ),
+    );
+
+    println!(
+        "{:?}",
+        get_stats_for_file(
+            &table,
+            std::string::String::from(
+                "part-00000-b5f1c7b4-7d81-4461-ac30-4446d48a53a4-c000.snappy.parquet"
+            )
+        )
+    );
+    fn count_not_null_stats(
+        stats_hashmap: &HashMap<String, deltalake::action::ColumnValueStat>,
+    ) -> usize {
+        stats_hashmap
+            .iter()
+            .filter(|(_, column_value_stat)| {
+                column_value_stat.as_value().unwrap().clone() != serde_json::Value::Null
+            })
+            .count()
+        }
+    assert_eq!(first_file_stats.num_records, 1)
+    assert_eq!(
+        first_file_stats.null_count.iter()
+        .filter(|(_, column_value_stat)| {
+            column_value_stat.as_value().unwrap().clone() != serde_json::Value::Null
+        })
+        .count(),
+        first_file_stats.null_count.len()
+    );
+    assert_eq!(
+        count_not_null_stats(&first_file_stats.min_values),
+        first_file_stats.min_values.len()
+    );
+    assert_eq!(
+        count_not_null_stats(&first_file_stats.max_values),
+        first_file_stats.max_values.len()
+    );
+}
+
 #[tokio::test]
 async fn test_action_reconciliation() {
     let path = "./tests/data/action_reconciliation";
