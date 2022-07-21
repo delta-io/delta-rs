cb62303914d8f9bca8e615d8ea568b4019b47124
Will Jones <willjones127@gmail.com>
2022-07-20 20:26:16 -0700

    feat: Move implementation into schema.rs

--- python/deltalake/schema.py
+++ python/deltalake/schema.py
@@ -1,62 +1,7 @@
-from dataclasses import dataclass
-from typing import Dict, Optional, Union
+from typing import Union
 
-import pyarrow as pa
-
-from .deltalake import ArrayType, MapType, StructType, PrimitiveType
-# For now, StructType and Schema are identical
-from .deltalake import StructType as Schema
+from .deltalake import ArrayType, Field, MapType, PrimitiveType, Schema, StructType
 
 # Can't implement inheritance (see note in src/schema.rs), so this is next
 # best thing.
-DataType = Union[PrimitiveType, MapType, StructType, ArrayType]
-
-@dataclass
-class Field:
-    """Create a DeltaTable Field instance."""
-
-    name: str
-    type: DataType
-    nullable: bool
-    metadata: Optional[Dict[str, str]] = None
-
-    def __str__(self) -> str:
-        return f"Field({self.name}: {self.type} nullable({self.nullable}) metadata({self.metadata}))"
-
-    def to_pyarrow(self) -> pa.Field:
-        return pa.field(
-            name=self.name,
-            type=self.type.to_pyarrow(),
-            nullable=self.nullable,
-            metadata=self.metadata,
-        )
-
-    @classmethod
-    def from_pyarrow(cls, field: pa.Field) -> "Field":
-        if isinstance(field.type, pa.StructType):
-            data_type = StructType.from_pyarrow(field.type)
-        elif isinstance(field.type, pa.ListType):
-            data_type = ArrayType.from_pyarrow(field.type)
-        elif isinstance(field.type, pa.MapType):
-            data_type = MapType.from_pyarrow(field.type)
-        else:
-            data_type = PrimitiveType.from_pyarrow(field.type)
-
-        return cls(
-            name=field.name,
-            type=data_type,
-            nullable=field.nullable,
-            metadata=field.metadata,
-        )
-
-class Schema(StructType):
-
-    def __str__(self) -> str:
-        pass
-
-    def to_pyarrow(self) -> pa.Schema:
-        return pa.schema(list(super().to_pyarrow()))
-
-    @classmethod
-    def from_pyarrow(cls, schema: pa.Schema) -> "Schema":
-        return super().from_pyarrow(list(schema))
+DataType = Union["PrimitiveType", "MapType", "StructType", "ArrayType"]
--- python/tests/test_schema.py
+++ python/tests/test_schema.py
@@ -1,17 +1,16 @@
 from array import array
-import pyarrow
 
+import pyarrow
 import pytest
 
 from deltalake import DeltaTable, Field
 from deltalake.schema import (
-    PrimitiveType,
     ArrayType,
     DataType,
     MapType,
+    PrimitiveType,
     Schema,
     StructType,
-    pyarrow_field_from_dict,
 )
 
 
@@ -26,13 +25,15 @@ def test_table_schema():
     assert len(schema.fields) == 1
     field = schema.fields[0]
     assert field.name == "id"
-    assert field.type == DataType("long")
+    assert field.type == PrimitiveType("long")
     assert field.nullable is True
     assert field.metadata == {}
 
     json = '{"type":"struct","fields":[{"name":"x","type":{"type":"array","elementType":"long","containsNull":true},"nullable":true,"metadata":{}}]}'
     schema = Schema.from_json(json)
-    assert schema.fields[0] == Field("x", ArrayType(DataType("long"), True), True, {})
+    assert schema.fields[0] == Field(
+        "x", ArrayType(PrimitiveType("long"), True), True, {}
+    )
 
 
 def test_table_schema_pyarrow_simple():
@@ -59,38 +60,38 @@ def test_table_schema_pyarrow_020():
     assert field.metadata is None
 
 
-def test_schema_pyarrow_from_decimal_and_floating_types():
-    field_name = "decimal_test"
-    metadata = {b"metadata_k": b"metadata_v"}
-    precision = 20
-    scale = 2
-    pyarrow_field = pyarrow_field_from_dict(
-        {
-            "name": field_name,
-            "nullable": False,
-            "metadata": metadata,
-            "type": {"name": "decimal", "precision": precision, "scale": scale},
-        }
-    )
-    assert pyarrow_field.name == field_name
-    assert pyarrow_field.type == pyarrow.decimal128(precision=precision, scale=scale)
-    assert dict(pyarrow_field.metadata) == metadata
-    assert pyarrow_field.nullable is False
+# def test_schema_pyarrow_from_decimal_and_floating_types():
+#     field_name = "decimal_test"
+#     metadata = {b"metadata_k": b"metadata_v"}
+#     precision = 20
+#     scale = 2
+#     pyarrow_field = pyarrow_field_from_dict(
+#         {
+#             "name": field_name,
+#             "nullable": False,
+#             "metadata": metadata,
+#             "type": {"name": "decimal", "precision": precision, "scale": scale},
+#         }
+#     )
+#     assert pyarrow_field.name == field_name
+#     assert pyarrow_field.type == pyarrow.decimal128(precision=precision, scale=scale)
+#     assert dict(pyarrow_field.metadata) == metadata
+#     assert pyarrow_field.nullable is False
 
-    field_name = "floating_test"
-    metadata = {b"metadata_k": b"metadata_v"}
-    pyarrow_field = pyarrow_field_from_dict(
-        {
-            "name": field_name,
-            "nullable": False,
-            "metadata": metadata,
-            "type": {"name": "floatingpoint", "precision": "HALF"},
-        }
-    )
-    assert pyarrow_field.name == field_name
-    assert pyarrow_field.type == pyarrow.float16()
-    assert dict(pyarrow_field.metadata) == metadata
-    assert pyarrow_field.nullable is False
+#     field_name = "floating_test"
+#     metadata = {b"metadata_k": b"metadata_v"}
+#     pyarrow_field = pyarrow_field_from_dict(
+#         {
+#             "name": field_name,
+#             "nullable": False,
+#             "metadata": metadata,
+#             "type": {"name": "floatingpoint", "precision": "HALF"},
+#         }
+#     )
+#     assert pyarrow_field.name == field_name
+#     assert pyarrow_field.type == pyarrow.float16()
+#     assert dict(pyarrow_field.metadata) == metadata
+#     assert pyarrow_field.nullable is False
 
 
 def test_primitive_delta_types():
@@ -109,11 +110,7 @@ def test_primitive_delta_types():
         "decimal(10,2)",
     ]
 
-    invalid_types = [
-        "int",
-        "decimal",
-        "decimal()"
-    ]
+    invalid_types = ["int", "decimal", "decimal()"]
 
     for data_type in valid_types:
         delta_type = PrimitiveType(data_type)
@@ -126,7 +123,7 @@ def test_primitive_delta_types():
 
         json_type = delta_type.to_json()
         assert delta_type == PrimitiveType.from_json(json_type)
-    
+
     for data_type in invalid_types:
         with pytest.raises(ValueError):
             PrimitiveType(data_type)
@@ -156,7 +153,11 @@ def test_map_delta_types():
     init_values = [
         (PrimitiveType("string"), PrimitiveType("decimal(20,9)"), False),
         (PrimitiveType("float"), PrimitiveType("string"), True),
-        (PrimitiveType("string"), MapType(PrimitiveType("date"), PrimitiveType("date"), True), False)
+        (
+            PrimitiveType("string"),
+            MapType(PrimitiveType("date"), PrimitiveType("date"), True),
+            False,
+        ),
     ]
     for key_type, value_type, value_contains_null in init_values:
         map_type = MapType(key_type, value_type, value_contains_null)
@@ -174,15 +175,16 @@ def test_map_delta_types():
         json_type = map_type.to_json()
         assert map_type == MapType.from_json(json_type)
 
+
 def test_struct_delta_types():
     fields = [
         Field("x", "integer", nullable=True, metadata={"x": {"y": 3}}),
-        Field("y", PrimitiveType("string"), nullable=False)
+        Field("y", PrimitiveType("string"), nullable=False),
     ]
 
     struct_type = StructType(fields)
 
-    assert struct_type.name == "struct"
+    assert struct_type.type == "struct"
     assert struct_type.fields == fields
 
     pa_type = struct_type.to_pyarrow()
@@ -191,6 +193,47 @@ def test_struct_delta_types():
     json_type = struct_type.to_json()
     assert struct_type == StructType.from_json(json_type)
 
+
+def test_delta_field():
+    args = [
+        ("x", PrimitiveType("string"), True, {}),
+        ("y", "float", False, {"x": {"y": 3}}),
+        ("z", ArrayType(StructType([Field("x", "integer", True)]), True), True, None),
+    ]
+
+    # TODO: are there field names we should reject?
+
+    for name, ty, nullable, metadata in args:
+        field = Field(name, ty, nullable=nullable, metadata=metadata)
+
+        assert field.name == name        
+        assert field.type == (PrimitiveType(ty) if isinstance(ty, str) else ty)
+        assert field.nullable == nullable
+        assert field.metadata == metadata
+
+        pa_field = field.to_pyarrow()
+        assert field == Field.from_pyarrow(pa_field)
+
+        json_field = field.to_json()
+        assert field == Field.from_json(json_field)
+
+
+def test_delta_schema():
+    fields = [
+        Field("x", "integer", nullable=True, metadata={"x": {"y": 3}}),
+        Field("y", PrimitiveType("string"), nullable=False),
+    ]
+
+    schema = Schema(fields)
+
+    assert schema.fields == fields
+
+    pa_schema = schema.to_pyarrow()
+    assert schema == Schema.from_pyarrow(pa_schema)
+
+    empty_schema = Schema([])
+    pa_schema = empty_schema.to_pyarrow()
+    assert schema == Schema.from_pyarrow(empty_schema)
 
 
 # def test_schema_delta_types():
@@ -262,167 +305,3 @@ def test_struct_delta_types():
 #     assert delta_field.type == StructType([Field("x", DataType("integer"), True, {})])
 #     assert delta_field.metadata == metadata
 #     assert delta_field.nullable is False
-
-
-def test_schema_pyarrow_types():
-    field_name = "column1"
-    metadata = {b"metadata_k": b"metadata_v"}
-    pyarrow_field = pyarrow_field_from_dict(
-        {
-            "name": field_name,
-            "nullable": False,
-            "metadata": metadata,
-            "type": {"name": "int", "bitWidth": 8, "isSigned": True},
-        }
-    )
-    assert pyarrow_field.name == field_name
-    assert pyarrow_field.type == pyarrow.int8()
-    assert dict(pyarrow_field.metadata) == metadata
-    assert pyarrow_field.nullable is False
-
-    field_name = "column_timestamp_no_unit"
-    metadata = {b"metadata_k": b"metadata_v"}
-    pyarrow_field = pyarrow_field_from_dict(
-        {
-            "name": field_name,
-            "nullable": False,
-            "metadata": metadata,
-            "type": {"name": "timestamp"},
-        }
-    )
-    assert pyarrow_field.name == field_name
-    assert pyarrow_field.type == pyarrow.timestamp("ns")
-    assert dict(pyarrow_field.metadata) == metadata
-    assert pyarrow_field.nullable is False
-
-    field_name = "column_timestamp_with_unit"
-    metadata = {b"metadata_k": b"metadata_v"}
-    pyarrow_field = pyarrow_field_from_dict(
-        {
-            "name": field_name,
-            "nullable": False,
-            "metadata": metadata,
-            "type": {"name": "timestamp", "unit": "MICROSECOND"},
-        }
-    )
-    assert pyarrow_field.name == field_name
-    assert pyarrow_field.type == pyarrow.timestamp("us")
-    assert dict(pyarrow_field.metadata) == metadata
-    assert pyarrow_field.nullable is False
-
-    field_name = "date_with_day_unit"
-    metadata = {b"metadata_k": b"metadata_v"}
-    pyarrow_field = pyarrow_field_from_dict(
-        {
-            "name": field_name,
-            "nullable": False,
-            "metadata": metadata,
-            "type": {"name": "date", "unit": "DAY"},
-        }
-    )
-    assert pyarrow_field.name == field_name
-    assert pyarrow_field.type == pyarrow.date32()
-    assert dict(pyarrow_field.metadata) == metadata
-    assert pyarrow_field.nullable is False
-
-    field_name = "simple_list"
-    pyarrow_field = pyarrow_field_from_dict(
-        {
-            "name": field_name,
-            "nullable": False,
-            "metadata": metadata,
-            "type": {"name": "list"},
-            "children": [{"type": {"name": "int", "bitWidth": 32, "isSigned": True}}],
-        }
-    )
-    assert pyarrow_field.name == field_name
-    assert pyarrow_field.type == pyarrow.list_(pyarrow.field("item", pyarrow.int32()))
-    assert pyarrow_field.metadata == metadata
-    assert pyarrow_field.nullable is False
-
-    field_name = "dictionary"
-    pyarrow_field = pyarrow_field_from_dict(
-        {
-            "name": field_name,
-            "nullable": False,
-            "metadata": metadata,
-            "type": {"name": "int", "bitWidth": 32, "isSigned": True},
-            "children": [],
-            "dictionary": {
-                "id": 0,
-                "indexType": {"name": "int", "bitWidth": 16, "isSigned": True},
-            },
-        }
-    )
-    assert pyarrow_field.name == field_name
-    assert pyarrow_field.type == pyarrow.map_(pyarrow.int16(), pyarrow.int32())
-    assert pyarrow_field.metadata == metadata
-    assert pyarrow_field.nullable is False
-
-    field_name = "struct_array"
-    pyarrow_field = pyarrow_field_from_dict(
-        {
-            "name": field_name,
-            "nullable": False,
-            "metadata": metadata,
-            "type": {"name": "list"},
-            "children": [],
-            "dictionary": {
-                "id": 0,
-                "indexType": {"name": "int", "bitWidth": 32, "isSigned": True},
-            },
-        }
-    )
-    assert pyarrow_field.name == field_name
-    assert pyarrow_field.type == pyarrow.map_(
-        pyarrow.int32(),
-        pyarrow.list_(
-            pyarrow.field(
-                "entries",
-                pyarrow.struct(
-                    [pyarrow.field("val", pyarrow.int32(), False, metadata)]
-                ),
-            )
-        ),
-    )
-    assert pyarrow_field.metadata == metadata
-    assert pyarrow_field.nullable is False
-
-    field_name = "simple_dictionary"
-    pyarrow_field = pyarrow_field_from_dict(
-        {
-            "name": field_name,
-            "metadata": {"metadata_k": "metadata_v"},
-            "nullable": False,
-            "type": {"name": "dictionary"},
-            "dictionary": {"indexType": {"type": {"name": "int", "bitWidth": 8}}},
-            "children": [{"type": {"name": "int", "bitWidth": 32}}],
-        }
-    )
-    assert pyarrow_field.name == field_name
-    assert pyarrow_field.type == pyarrow.map_(pyarrow.int8(), pyarrow.int32())
-    assert pyarrow_field.metadata == metadata
-    assert pyarrow_field.nullable is False
-
-    pyarrow_field = pyarrow_field_from_dict(
-        {
-            "name": field_name,
-            "type": {"name": "struct"},
-            "children": [
-                {
-                    "name": "x",
-                    "type": {"name": "int", "bitWidth": 64},
-                    "nullable": True,
-                    "metadata": {},
-                }
-            ],
-            "metadata": {"metadata_k": "metadata_v"},
-            "nullable": False,
-        }
-    )
-    assert pyarrow_field.name == field_name
-    assert pyarrow_field.type == pyarrow.struct(
-        [pyarrow.field("x", pyarrow.int64(), True, {})]
-    )
-    assert pyarrow_field.metadata == metadata
-    assert pyarrow_field.nullable is False
