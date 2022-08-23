e4e5edada3379a8254523f0dcd5a576ad66a3d84
Robert Pack <robstar.pack@gmail.com>
2022-08-23 23:50:18 +0200

    more build

--- rust/tests/common/adls.rs
+++ rust/tests/common/adls.rs
@@ -56,8 +56,7 @@ pub async fn setup_azure_gen2_context() -> TestContext {
 }
 
 pub mod az_cli {
-    use super::set_env_if_not_set;
-    use crate::builder::azure_storage_options;
+    use deltalake::builder::azure_storage_options;
     use std::process::{Command, ExitStatus};
 
     /// Create a new bucket
@@ -88,20 +87,6 @@ pub mod az_cli {
             .spawn()
             .expect("az command is installed");
         child.wait()
-    }
-
-    /// prepare_env
-    pub fn prepare_env() {
-        set_env_if_not_set(azure_storage_options::AZURE_STORAGE_USE_EMULATOR, "1");
-        set_env_if_not_set(
-            azure_storage_options::AZURE_STORAGE_ACCOUNT_NAME,
-            "devstoreaccount1",
-        );
-        set_env_if_not_set(azure_storage_options::AZURE_STORAGE_ACCOUNT_KEY, "Eby8vdM02xNOcqFlqUwJPLlmEtlCDXJ1OUzFT50uSRZ6IFsuFq2UVErCz4I6tq/K1SZFPTOtr/KBHBeksoGMGw==");
-        set_env_if_not_set(
-            "AZURE_STORAGE_CONNECTION_STRING",
-            "DefaultEndpointsProtocol=http;AccountName=devstoreaccount1;AccountKey=Eby8vdM02xNOcqFlqUwJPLlmEtlCDXJ1OUzFT50uSRZ6IFsuFq2UVErCz4I6tq/K1SZFPTOtr/KBHBeksoGMGw==;BlobEndpoint=http://localhost:10000/devstoreaccount1;"
-        );
     }
 
     pub fn upload_table(src: &str, dst: &str) -> std::io::Result<ExitStatus> {
