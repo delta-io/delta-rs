90b68699e0266a99cb86c1680d7a51291fd7e964
Robert Pack <robstar.pack@gmail.com>
2022-12-13 23:40:13 +0100

    fix: type hint

--- python/deltalake/fs.py
+++ python/deltalake/fs.py
@@ -1,3 +1,5 @@
+from typing import Dict, List, Optional
+
 import pyarrow as pa
 from pyarrow.fs import FileInfo, FileSelector, FileSystemHandler
 
@@ -17,13 +19,13 @@ class DeltaStorageHandler(DeltaFileSystemHandler, FileSystemHandler):
         return pa.PythonFile(DeltaFileSystemHandler.open_input_file(self, path))
 
     def open_output_stream(
-        self, path: str, metadata: dict[str, str] | None = None
+        self, path: str, metadata: Optional[Dict[str, str]] = None
     ) -> pa.PythonFile:
         return pa.PythonFile(
             DeltaFileSystemHandler.open_output_stream(self, path, metadata)
         )
 
-    def get_file_info_selector(self, selector: FileSelector) -> list[FileInfo]:
+    def get_file_info_selector(self, selector: FileSelector) -> List[FileInfo]:  # type: ignore
         return DeltaFileSystemHandler.get_file_info_selector(
             self, selector.base_dir, selector.allow_not_found, selector.recursive
         )
