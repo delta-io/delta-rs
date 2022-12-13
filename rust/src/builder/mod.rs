b1b566e00175ff3d6777a1e38bf8d0ab32df6ca5
Robert Pack <robstar.pack@gmail.com>
2022-12-13 02:39:52 +0100

    fix: move profile code behind feature flag

--- rust/src/builder/mod.rs
+++ rust/src/builder/mod.rs
@@ -694,6 +694,7 @@ pub fn get_s3_builder_from_options(
     }
     builder = builder.with_region(s3_options.region.name());
 
+    #[cfg(feature = "aws-profile")]
     if let Some(profile) = &s3_options.profile {
         builder = builder.with_profile(profile);
     }
