1758c616fe39953c2578c3b41807688dbda0b855
Robert Pack <robstar.pack@gmail.com>
2022-12-14 00:14:03 +0100

    docs: docstrings

--- python/deltalake/fs.py
+++ python/deltalake/fs.py
@@ -13,19 +13,46 @@ class DeltaStorageHandler(DeltaFileSystemHandler, FileSystemHandler):
     """
 
     def open_input_file(self, path: str) -> pa.PythonFile:
+        """
+        Open an input file for random access reading.
+
+        :param source: The source to open for reading.
+        :return:  NativeFile
+        """
         return pa.PythonFile(DeltaFileSystemHandler.open_input_file(self, path))
 
     def open_input_stream(self, path: str) -> pa.PythonFile:
+        """
+        Open an input stream for sequential reading.
+
+        :param source: The source to open for reading.
+        :return:  NativeFile
+        """
         return pa.PythonFile(DeltaFileSystemHandler.open_input_file(self, path))
 
     def open_output_stream(
         self, path: str, metadata: Optional[Dict[str, str]] = None
     ) -> pa.PythonFile:
+        """
+        Open an output stream for sequential writing.
+
+        If the target already exists, existing data is truncated.
+
+        :param path: The source to open for writing.
+        :param metadata: If not None, a mapping of string keys to string values.
+        :return:  NativeFile
+        """
         return pa.PythonFile(
             DeltaFileSystemHandler.open_output_stream(self, path, metadata)
         )
 
     def get_file_info_selector(self, selector: FileSelector) -> List[FileInfo]:  # type: ignore
+        """
+        Get info for the files defined by FileSelector.
+
+        :param selector: FileSelector object
+        :return: list of file info objects
+        """
         return DeltaFileSystemHandler.get_file_info_selector(
             self, selector.base_dir, selector.allow_not_found, selector.recursive
         )
