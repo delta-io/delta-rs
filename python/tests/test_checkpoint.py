4c3217b63085b1a951d39685eab8441bc87aa162
Ilya Moshkov <ilya.moshkov@exosfinancial.com>
2023-01-10 22:59:49 +0400

    fix after format check

--- python/tests/test_checkpoint.py
+++ python/tests/test_checkpoint.py
@@ -1,12 +1,16 @@
 import pathlib
+
 import pyarrow as pa
+
 from deltalake import DeltaTable, write_deltalake
 
 
 def test_checkpoint(tmp_path: pathlib.Path, sample_data: pa.Table):
     tmp_table_path = tmp_path / "path" / "to" / "table"
     checkpoint_path = tmp_table_path / "_delta_log" / "_last_checkpoint"
-    last_checkpoint_path = tmp_table_path / "_delta_log" / "00000000000000000000.checkpoint.parquet"
+    last_checkpoint_path = (
+        tmp_table_path / "_delta_log" / "00000000000000000000.checkpoint.parquet"
+    )
 
     # TODO: Include decimal after fixing issue "Json error: Decimal128(5, 3) type is not supported"
     sample_data = sample_data.drop(["decimal"])
