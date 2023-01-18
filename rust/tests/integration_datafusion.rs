d90ddde4cdf4cbda6b29a0865d47875142bba0d2
David Blajda <db@davidblajda.com>
2023-01-17 22:45:33 -0500

    Demo issues with statistics for datafusion

--- rust/tests/integration_datafusion.rs
+++ rust/tests/integration_datafusion.rs
@@ -2,11 +2,147 @@
 
 use arrow::array::Int64Array;
 use common::datafusion::context_with_delta_table_factory;
+use datafusion::prelude::SessionContext;
+use deltalake::DeltaTableBuilder;
 use deltalake::test_utils::{IntegrationContext, StorageIntegration, TestResult, TestTables};
 use serial_test::serial;
 use std::sync::Arc;
+use url::Url;
+
+use arrow::datatypes::Schema as ArrowSchema;
+use arrow::{
+    array::{Int32Array, StringArray},
+    datatypes::{DataType, Field},
+    record_batch::RecordBatch,
+};
+use deltalake::optimize::{MetricDetails, Metrics};
+use deltalake::DeltaTableError;
+use deltalake::{
+    action,
+    action::Remove,
+    optimize::{create_merge_plan, Optimize},
+    writer::{DeltaWriter, RecordBatchWriter},
+    DeltaTableMetaData, PartitionFilter,
+};
+use deltalake::{DeltaTable, Schema, SchemaDataType, SchemaField};
+use rand::prelude::*;
+use serde_json::{json, Map, Value};
+use std::time::SystemTime;
+use std::time::UNIX_EPOCH;
+use std::{collections::HashMap, error::Error};
+use tempdir::TempDir;
+
+
 
 mod common;
+fn tuples_to_batch<T: Into<String>>(
+    tuples: Vec<(i32, i32)>,
+    partition: T,
+) -> Result<RecordBatch, Box<dyn Error>> {
+    let mut x_vec: Vec<i32> = Vec::new();
+    let mut y_vec: Vec<i32> = Vec::new();
+    let mut date_vec = Vec::new();
+    let s = partition.into();
+
+    for t in tuples {
+        x_vec.push(t.0);
+        y_vec.push(t.1);
+        date_vec.push(s.clone());
+    }
+
+    let x_array = Int32Array::from(x_vec);
+    let y_array = Int32Array::from(y_vec);
+    let date_array = StringArray::from(date_vec);
+
+    Ok(RecordBatch::try_new(
+        Arc::new(ArrowSchema::new(vec![
+            Field::new("x", DataType::Int32, false),
+            Field::new("y", DataType::Int32, false),
+            Field::new("date", DataType::Utf8, false),
+        ])),
+        vec![Arc::new(x_array), Arc::new(y_array), Arc::new(date_array)],
+    )?)
+}
+
+
+struct Context {
+    pub tmp_dir: TempDir,
+    pub table: DeltaTable,
+}
+
+async fn setup_test(partitioned: bool) -> Result<Context, Box<dyn Error>> {
+    let schema = Schema::new(vec![
+        SchemaField::new(
+            "x".to_owned(),
+            SchemaDataType::primitive("integer".to_owned()),
+            false,
+            HashMap::new(),
+        ),
+        SchemaField::new(
+            "y".to_owned(),
+            SchemaDataType::primitive("integer".to_owned()),
+            false,
+            HashMap::new(),
+        ),
+        SchemaField::new(
+            "date".to_owned(),
+            SchemaDataType::primitive("string".to_owned()),
+            false,
+            HashMap::new(),
+        ),
+    ]);
+
+    let p = if partitioned {
+        vec!["date".to_owned()]
+    } else {
+        vec![]
+    };
+
+    let table_meta = DeltaTableMetaData::new(
+        Some("opt_table".to_owned()),
+        Some("Table for optimize tests".to_owned()),
+        None,
+        schema.clone(),
+        p,
+        HashMap::new(),
+    );
+
+    let tmp_dir = tempdir::TempDir::new("opt_table").unwrap();
+    let p = tmp_dir.path().to_str().to_owned().unwrap();
+    let mut dt = DeltaTableBuilder::from_uri(p).build()?;
+
+    let mut commit_info = Map::<String, Value>::new();
+
+    let protocol = action::Protocol {
+        min_reader_version: 1,
+        min_writer_version: 2,
+    };
+
+    commit_info.insert(
+        "operation".to_string(),
+        serde_json::Value::String("CREATE TABLE".to_string()),
+    );
+    dt.create(
+        table_meta.clone(),
+        protocol.clone(),
+        Some(commit_info),
+        None,
+    )
+    .await?;
+
+    Ok(Context { tmp_dir, table: dt })
+}
+
+pub async fn write(
+    writer: &mut RecordBatchWriter,
+    table: &mut DeltaTable,
+    batch: RecordBatch,
+) -> Result<(), DeltaTableError> {
+    writer.write(batch).await?;
+    writer.flush_and_commit(table).await?;
+    Ok(())
+}
+
 
 #[tokio::test]
 #[serial]
@@ -33,6 +169,32 @@ async fn test_datafusion_azure() -> TestResult {
 #[serial]
 async fn test_datafusion_gcp() -> TestResult {
     Ok(test_datafusion(StorageIntegration::Google).await?)
+}
+
+#[tokio::test]
+#[serial]
+async fn test_datafusion_table_with_statistics() -> TestResult {
+    let context = setup_test(true).await?;
+    let mut dt = context.table;
+    let mut writer = RecordBatchWriter::for_table(&dt)?;
+
+    write(
+        &mut writer,
+        &mut dt,
+        tuples_to_batch(vec![(1, 2), (1, 3), (1, 4)], "2022-05-22")?,
+    )
+    .await?;
+
+    let ctx = SessionContext::new();
+    ctx.register_table("demo", Arc::new(dt))?;
+
+    let batches = ctx
+        .sql("SELECT count(*) FROM demo WHERE date = '2022-05-22'")
+        .await?
+        .collect()
+        .await?;
+
+    Ok(())
 }
 
 async fn test_datafusion(storage: StorageIntegration) -> TestResult {
