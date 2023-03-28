f5b3482243cf1c82c037ea863561b15c12b4c37d
Will Jones <willjones127@gmail.com>
2023-03-27 21:42:45 -0700

    feat: add package version (#1243)
    # Description
    
    Low maintenance way to expose package version. It will come from the
    `cargo.toml`.
    
    Note that we have a separate function already called `rust_core_version`
    that provides the version of the `deltalake` Rust crate. That currently
    matches the Python library, but I don't think we provide any guarantee
    it will continue to.
    
    # Related Issue(s)
    
    For example:
    
    - closes #1227 
    
    
    # Documentation
    
    <!---
    Share links to useful documentation
    --->

--- python/deltalake/__init__.py
+++ python/deltalake/__init__.py
@@ -1,4 +1,4 @@
-from ._internal import PyDeltaTableError, RawDeltaTable, rust_core_version
+from ._internal import PyDeltaTableError, RawDeltaTable, __version__, rust_core_version
 from .data_catalog import DataCatalog
 from .schema import DataType, Field, Schema
 from .table import DeltaTable, Metadata
