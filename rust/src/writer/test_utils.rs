2e0d72e4c3a2402c7899b7d6e5e2e5ddef78970c
R. Tyler Croy <rtyler@brokenco.de>
2023-04-13 20:19:19 -0700

    Introduce a test function to make it easier to create test tables with schemas

--- rust/src/writer/test_utils.rs
+++ rust/src/writer/test_utils.rs
@@ -164,6 +164,10 @@ pub fn get_delta_metadata(partition_cols: &[String]) -> DeltaTableMetaData {
     )
 }
 
+/*
+ * Create a bare table without any commits, this basically just creates a directory and then
+ * initializes a DeltaTable from it
+ */
 pub fn create_bare_table() -> DeltaTable {
     let table_dir = tempfile::tempdir().unwrap();
     let table_path = table_dir.path();
@@ -172,10 +176,14 @@ pub fn create_bare_table() -> DeltaTable {
         .unwrap()
 }
 
-pub async fn create_initialized_table(partition_cols: &[String]) -> DeltaTable {
+/*
+ * Create an initialized table with a schema
+ */
+pub async fn create_initialized_table_with(
+    schema: Schema,
+    partition_cols: &[String],
+) -> DeltaTable {
     let mut table = create_bare_table();
-    let table_schema = get_delta_schema();
-
     let mut commit_info = serde_json::Map::<String, serde_json::Value>::new();
     commit_info.insert(
         "operation".to_string(),
@@ -195,7 +203,7 @@ pub async fn create_initialized_table(partition_cols: &[String]) -> DeltaTable {
         None,
         None,
         None,
-        table_schema,
+        schema,
         partition_cols.to_vec(),
         HashMap::new(),
     );
@@ -206,4 +214,12 @@ pub async fn create_initialized_table(partition_cols: &[String]) -> DeltaTable {
         .unwrap();
 
     table
+}
+
+/*
+ * Create an initialize table in a temp directory with a test schema
+ */
+pub async fn create_initialized_table(partition_cols: &[String]) -> DeltaTable {
+    let table_schema = get_delta_schema();
+    create_initialized_table_with(table_schema, partition_cols).await
 }
