d0ea3bc48c57872ebb19be909f3e65b9b2896a36
Jan Noha <nohajc@gmail.com>
2023-05-01 00:01:59 +0200

    added support for Databricks Unity Catalog

--- python/deltalake/data_catalog.py
+++ python/deltalake/data_catalog.py
@@ -4,4 +4,5 @@ from enum import Enum
 class DataCatalog(Enum):
     """List of the Data Catalogs"""
 
-    AWS = "glue"  # Only AWS Glue Data Catalog is available
+    AWS = "glue"  # AWS Glue Data Catalog
+    UNITY = "unity" # Databricks Unity Catalog
