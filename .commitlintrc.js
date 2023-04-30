a07beb5f10137531b5e791c8adbc5c06909cbb73
Will Jones <willjones127@gmail.com>
2023-04-30 10:49:44 -0700

    fix the config file

--- .commitlintrc.js
+++ .commitlintrc.js
@@ -1,49 +1,15 @@
-const Configuration = {
-    /*
-     * Resolve and load @commitlint/config-conventional from node_modules.
-     * Referenced packages must be installed
-     */
+module.exports = {
     extends: ['@commitlint/config-conventional'],
-    /*
-     * Resolve and load conventional-changelog-atom from node_modules.
-     * Referenced packages must be installed
-     */
-    parserPreset: 'conventional-changelog-atom',
-    /*
-     * Resolve and load @commitlint/format from node_modules.
-     * Referenced package must be installed
-     */
-    formatter: '@commitlint/format',
-    /*
-     * Any rules defined here will override rules from @commitlint/config-conventional
-     */
+    // Workaround for https://github.com/dependabot/dependabot-core/issues/5923
+    ignores: [(message) => /^Bumps \[.+]\(.+\) from .+ to .+\.$/m.test(message)],
     rules: {
-        'type-enum': [2, 'always', ['foo']],
+        'body-max-length': [0, 'always'],
+        'body-max-line-length': [0, 'always'],
+        'footer-max-length': [0, 'always'],
+        'footer-max-line-length': [0, 'always'],
+        'header-max-length': [0, 'always'],
+        'scope-max-length': [0, 'always'],
+        'subject-max-length': [0, 'always'],
+        'type-max-length': [0, 'always'],
     },
-    /*
-     * Functions that return true if commitlint should ignore the given message.
-     */
-    ignores: [(commit) => commit === ''],
-    /*
-     * Whether commitlint uses the default ignore rules.
-     */
-    defaultIgnores: true,
-    /*
-     * Custom URL to show upon failure
-     */
-    helpUrl:
-        'https://github.com/conventional-changelog/commitlint/#what-is-commitlint',
-    /*
-     * Custom prompt configs
-     */
-    prompt: {
-        messages: {},
-        questions: {
-            type: {
-                description: 'please input type:',
-            },
-        },
-    },
-};
-
-module.exports = Configuration;
+}
