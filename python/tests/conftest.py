cbed28ad22b9d44d109d0a4e329a08dfc6221f3e
R. Tyler Croy <rtyler@brokenco.de>
2023-10-25 21:40:08 -0700

    chore: refactor into the deltalake meta crate and deltalake-core crates
    This puts the groundwork in place for starting to partition into smaller crates
    in a simpler and more manageable fashion.
    
    See #1713

--- python/tests/conftest.py
+++ python/tests/conftest.py
@@ -58,7 +58,7 @@ def s3_localstack_creds():
             "s3",
             "sync",
             "--quiet",
-            "../rust/tests/data/simple_table",
+            "../crates/deltalake-core/tests/data/simple_table",
             "s3://deltars/simple",
             "--endpoint-url",
             endpoint_url,
