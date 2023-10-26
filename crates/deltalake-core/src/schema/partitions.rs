d5ad4fb3853f88c2d821af2a6cf162b0f83d9ad0
R. Tyler Croy <rtyler@brokenco.de>
2023-10-25 21:40:08 -0700

    chore: refactor into the deltalake meta crate and deltalake-core crates
    This puts the groundwork in place for starting to partition into smaller crates
    in a simpler and more manageable fashion.
    
    See #1713

---
+++ crates/deltalake-core/src/schema/partitions.rs
@@ -0,0 +1,265 @@
+//! Delta Table partition handling logic.
+
+use std::convert::TryFrom;
+
+use super::SchemaDataType;
+use crate::errors::DeltaTableError;
+use std::cmp::Ordering;
+use std::collections::HashMap;
+
+/// A Enum used for selecting the partition value operation when filtering a DeltaTable partition.
+#[derive(Clone, Debug, PartialEq, Eq)]
+pub enum PartitionValue {
+    /// The partition value with the equal operator
+    Equal(String),
+    /// The partition value with the not equal operator
+    NotEqual(String),
+    /// The partition value with the greater than operator
+    GreaterThan(String),
+    /// The partition value with the greater than or equal operator
+    GreaterThanOrEqual(String),
+    /// The partition value with the less than operator
+    LessThan(String),
+    /// The partition value with the less than or equal operator
+    LessThanOrEqual(String),
+    /// The partition values with the in operator
+    In(Vec<String>),
+    /// The partition values with the not in operator
+    NotIn(Vec<String>),
+}
+
+/// A Struct used for filtering a DeltaTable partition by key and value.
+#[derive(Clone, Debug, PartialEq, Eq)]
+pub struct PartitionFilter {
+    /// The key of the PartitionFilter
+    pub key: String,
+    /// The value of the PartitionFilter
+    pub value: PartitionValue,
+}
+
+fn compare_typed_value(
+    partition_value: &str,
+    filter_value: &str,
+    data_type: &SchemaDataType,
+) -> Option<Ordering> {
+    match data_type {
+        SchemaDataType::primitive(primitive_type) => match primitive_type.as_str() {
+            "long" | "integer" | "short" | "byte" => match filter_value.parse::<i64>() {
+                Ok(parsed_filter_value) => {
+                    let parsed_partition_value = partition_value.parse::<i64>().unwrap();
+                    parsed_partition_value.partial_cmp(&parsed_filter_value)
+                }
+                _ => None,
+            },
+            "float" | "double" => match filter_value.parse::<f64>() {
+                Ok(parsed_filter_value) => {
+                    let parsed_partition_value = partition_value.parse::<f64>().unwrap();
+                    parsed_partition_value.partial_cmp(&parsed_filter_value)
+                }
+                _ => None,
+            },
+            _ => partition_value.partial_cmp(filter_value),
+        },
+        _ => partition_value.partial_cmp(filter_value),
+    }
+}
+
+/// Partition filters methods for filtering the DeltaTable partitions.
+impl PartitionFilter {
+    /// Indicates if a DeltaTable partition matches with the partition filter by key and value.
+    pub fn match_partition(
+        &self,
+        partition: &DeltaTablePartition,
+        data_type: &SchemaDataType,
+    ) -> bool {
+        if self.key != partition.key {
+            return false;
+        }
+
+        match &self.value {
+            PartitionValue::Equal(value) => value == &partition.value,
+            PartitionValue::NotEqual(value) => value != &partition.value,
+            PartitionValue::GreaterThan(value) => {
+                compare_typed_value(&partition.value, value, data_type)
+                    .map(|x| x.is_gt())
+                    .unwrap_or(false)
+            }
+            PartitionValue::GreaterThanOrEqual(value) => {
+                compare_typed_value(&partition.value, value, data_type)
+                    .map(|x| x.is_ge())
+                    .unwrap_or(false)
+            }
+            PartitionValue::LessThan(value) => {
+                compare_typed_value(&partition.value, value, data_type)
+                    .map(|x| x.is_lt())
+                    .unwrap_or(false)
+            }
+            PartitionValue::LessThanOrEqual(value) => {
+                compare_typed_value(&partition.value, value, data_type)
+                    .map(|x| x.is_le())
+                    .unwrap_or(false)
+            }
+            PartitionValue::In(value) => value.contains(&partition.value),
+            PartitionValue::NotIn(value) => !value.contains(&partition.value),
+        }
+    }
+
+    /// Indicates if one of the DeltaTable partition among the list
+    /// matches with the partition filter.
+    pub fn match_partitions(
+        &self,
+        partitions: &[DeltaTablePartition],
+        partition_col_data_types: &HashMap<&str, &SchemaDataType>,
+    ) -> bool {
+        let data_type = partition_col_data_types
+            .get(self.key.as_str())
+            .unwrap()
+            .to_owned();
+        partitions
+            .iter()
+            .any(|partition| self.match_partition(partition, data_type))
+    }
+}
+
+/// Create a PartitionFilter from a filter Tuple with the structure (key, operation, value).
+impl TryFrom<(&str, &str, &str)> for PartitionFilter {
+    type Error = DeltaTableError;
+
+    /// Try to create a PartitionFilter from a Tuple of (key, operation, value).
+    /// Returns a DeltaTableError in case of a malformed filter.
+    fn try_from(filter: (&str, &str, &str)) -> Result<Self, DeltaTableError> {
+        match filter {
+            (key, "=", value) if !key.is_empty() => Ok(PartitionFilter {
+                key: key.to_owned(),
+                value: PartitionValue::Equal(value.to_owned()),
+            }),
+            (key, "!=", value) if !key.is_empty() => Ok(PartitionFilter {
+                key: key.to_owned(),
+                value: PartitionValue::NotEqual(value.to_owned()),
+            }),
+            (key, ">", value) if !key.is_empty() => Ok(PartitionFilter {
+                key: key.to_owned(),
+                value: PartitionValue::GreaterThan(value.to_owned()),
+            }),
+            (key, ">=", value) if !key.is_empty() => Ok(PartitionFilter {
+                key: key.to_owned(),
+                value: PartitionValue::GreaterThanOrEqual(value.to_owned()),
+            }),
+            (key, "<", value) if !key.is_empty() => Ok(PartitionFilter {
+                key: key.to_owned(),
+                value: PartitionValue::LessThan(value.to_owned()),
+            }),
+            (key, "<=", value) if !key.is_empty() => Ok(PartitionFilter {
+                key: key.to_owned(),
+                value: PartitionValue::LessThanOrEqual(value.to_owned()),
+            }),
+            (_, _, _) => Err(DeltaTableError::InvalidPartitionFilter {
+                partition_filter: format!("{filter:?}"),
+            }),
+        }
+    }
+}
+
+/// Create a PartitionFilter from a filter Tuple with the structure (key, operation, list(value)).
+impl TryFrom<(&str, &str, &[&str])> for PartitionFilter {
+    type Error = DeltaTableError;
+
+    /// Try to create a PartitionFilter from a Tuple of (key, operation, list(value)).
+    /// Returns a DeltaTableError in case of a malformed filter.
+    fn try_from(filter: (&str, &str, &[&str])) -> Result<Self, DeltaTableError> {
+        match filter {
+            (key, "in", value) if !key.is_empty() => Ok(PartitionFilter {
+                key: key.to_owned(),
+                value: PartitionValue::In(value.iter().map(|x| x.to_string()).collect()),
+            }),
+            (key, "not in", value) if !key.is_empty() => Ok(PartitionFilter {
+                key: key.to_owned(),
+                value: PartitionValue::NotIn(value.iter().map(|x| x.to_string()).collect()),
+            }),
+            (_, _, _) => Err(DeltaTableError::InvalidPartitionFilter {
+                partition_filter: format!("{filter:?}"),
+            }),
+        }
+    }
+}
+
+/// A Struct DeltaTablePartition used to represent a partition of a DeltaTable.
+#[derive(Clone, Debug, PartialEq, Eq)]
+pub struct DeltaTablePartition {
+    /// The key of the DeltaTable partition.
+    pub key: String,
+    /// The value of the DeltaTable partition.
+    pub value: String,
+}
+
+/// Create a DeltaTable partition from a HivePartition string.
+///
+/// A HivePartition string is represented by a "key=value" format.
+///
+/// ```rust
+/// use deltalake_core::DeltaTablePartition;
+///
+/// let hive_part = "ds=2023-01-01";
+/// let partition = DeltaTablePartition::try_from(hive_part).unwrap();
+/// assert_eq!("ds", partition.key);
+/// assert_eq!("2023-01-01", partition.value);
+/// ```
+impl TryFrom<&str> for DeltaTablePartition {
+    type Error = DeltaTableError;
+
+    /// Try to create a DeltaTable partition from a HivePartition string.
+    /// Returns a DeltaTableError if the string is not in the form of a HivePartition.
+    fn try_from(partition: &str) -> Result<Self, DeltaTableError> {
+        let partition_splitted: Vec<&str> = partition.split('=').collect();
+        match partition_splitted {
+            partition_splitted if partition_splitted.len() == 2 => Ok(DeltaTablePartition {
+                key: partition_splitted[0].to_owned(),
+                value: partition_splitted[1].to_owned(),
+            }),
+            _ => Err(DeltaTableError::PartitionError {
+                partition: partition.to_string(),
+            }),
+        }
+    }
+}
+
+impl DeltaTablePartition {
+    /// Try to create a DeltaTable partition from a partition value kv pair.
+    ///
+    /// ```rust
+    /// use deltalake_core::DeltaTablePartition;
+    ///
+    /// let value = ("ds", &Some("2023-01-01".to_string()));
+    /// let null_default = "1979-01-01";
+    /// let partition = DeltaTablePartition::from_partition_value(value, null_default);
+    ///
+    /// assert_eq!("ds", partition.key);
+    /// assert_eq!("2023-01-01", partition.value);
+    /// ```
+    pub fn from_partition_value(
+        partition_value: (&str, &Option<String>),
+        default_for_null: &str,
+    ) -> Self {
+        let (k, v) = partition_value;
+        let v = match v {
+            Some(s) => s,
+            None => default_for_null,
+        };
+        DeltaTablePartition {
+            key: k.to_owned(),
+            value: v.to_owned(),
+        }
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn tryfrom_invalid() {
+        let buf = "this-is-not-a-partition";
+        let partition = DeltaTablePartition::try_from(buf);
+        assert!(partition.is_err());
+    }
+}
---
+++ crates/deltalake-core/tests/command_filesystem_check.rs
@@ -0,0 +1,177 @@
+#![cfg(feature = "integration_test")]
+
+use deltalake_core::test_utils::{
+    set_env_if_not_set, IntegrationContext, StorageIntegration, TestResult, TestTables,
+};
+use deltalake_core::Path;
+use deltalake_core::{errors::DeltaTableError, DeltaOps};
+use serial_test::serial;
+
+mod common;
+
+#[tokio::test]
+#[serial]
+async fn test_filesystem_check_local() -> TestResult {
+    test_filesystem_check(StorageIntegration::Local).await
+}
+
+#[cfg(any(feature = "s3", feature = "s3-native-tls"))]
+#[tokio::test]
+#[serial]
+async fn test_filesystem_check_aws() -> TestResult {
+    set_env_if_not_set("AWS_S3_ALLOW_UNSAFE_RENAME", "true");
+    set_env_if_not_set("AWS_S3_LOCKING_PROVIDER", "none");
+    test_filesystem_check(StorageIntegration::Amazon).await
+}
+
+#[cfg(feature = "azure")]
+#[tokio::test]
+#[serial]
+async fn test_filesystem_check_azure() -> TestResult {
+    test_filesystem_check(StorageIntegration::Microsoft).await
+}
+
+#[cfg(feature = "gcs")]
+#[tokio::test]
+#[serial]
+async fn test_filesystem_check_gcp() -> TestResult {
+    test_filesystem_check(StorageIntegration::Google).await
+}
+
+#[cfg(feature = "hdfs")]
+#[tokio::test]
+#[serial]
+async fn test_filesystem_check_hdfs() -> TestResult {
+    Ok(test_filesystem_check(StorageIntegration::Hdfs).await?)
+}
+
+async fn test_filesystem_check(storage: StorageIntegration) -> TestResult {
+    let context = IntegrationContext::new(storage)?;
+    context.load_table(TestTables::Simple).await?;
+    let file = "part-00000-2befed33-c358-4768-a43c-3eda0d2a499d-c000.snappy.parquet";
+    let path = Path::from_iter([&TestTables::Simple.as_name(), file]);
+
+    // Delete an active file from underlying storage without an update to the log to simulate an external fault
+    context.object_store().delete(&path).await?;
+
+    let table = context.table_builder(TestTables::Simple).load().await?;
+    let version = table.state.version();
+    let active = table.state.files().len();
+
+    // Validate a Dry run does not mutate the table log and indentifies orphaned add actions
+    let op = DeltaOps::from(table);
+    let (table, metrics) = op.filesystem_check().with_dry_run(true).await?;
+    assert_eq!(version, table.state.version());
+    assert_eq!(active, table.state.files().len());
+    assert_eq!(vec![file.to_string()], metrics.files_removed);
+
+    // Validate a run updates the table version with proper remove actions
+    let op = DeltaOps::from(table);
+    let (table, metrics) = op.filesystem_check().await?;
+    assert_eq!(version + 1, table.state.version());
+    assert_eq!(active - 1, table.state.files().len());
+    assert_eq!(vec![file.to_string()], metrics.files_removed);
+
+    let remove = table.state.all_tombstones().get(file).unwrap();
+    assert!(remove.data_change);
+
+    // An additonal run should return an empty list of orphaned actions
+    let op = DeltaOps::from(table);
+    let (table, metrics) = op.filesystem_check().await?;
+    assert_eq!(version + 1, table.state.version());
+    assert_eq!(active - 1, table.state.files().len());
+    assert!(metrics.files_removed.is_empty());
+
+    Ok(())
+}
+
+#[tokio::test]
+#[serial]
+async fn test_filesystem_check_partitioned() -> TestResult {
+    let storage = StorageIntegration::Local;
+    let context = IntegrationContext::new(storage)?;
+    context
+        .load_table(TestTables::Delta0_8_0Partitioned)
+        .await?;
+    let file = "year=2021/month=12/day=4/part-00000-6dc763c0-3e8b-4d52-b19e-1f92af3fbb25.c000.snappy.parquet";
+    let path = Path::parse(TestTables::Delta0_8_0Partitioned.as_name() + "/" + file).unwrap();
+
+    // Delete an active file from underlying storage without an update to the log to simulate an external fault
+    context.object_store().delete(&path).await?;
+
+    let table = context
+        .table_builder(TestTables::Delta0_8_0Partitioned)
+        .load()
+        .await?;
+    let version = table.state.version();
+    let active = table.state.files().len();
+
+    // Validate a run updates the table version with proper remove actions
+    let op = DeltaOps::from(table);
+    let (table, metrics) = op.filesystem_check().await?;
+    assert_eq!(version + 1, table.state.version());
+    assert_eq!(active - 1, table.state.files().len());
+    assert_eq!(vec![file.to_string()], metrics.files_removed);
+
+    let remove = table.state.all_tombstones().get(file).unwrap();
+    assert!(remove.data_change);
+    Ok(())
+}
+
+#[tokio::test]
+#[serial]
+async fn test_filesystem_check_fails_for_concurrent_delete() -> TestResult {
+    // Validate failure when a non dry only executes on the latest version
+    let context = IntegrationContext::new(StorageIntegration::Local)?;
+    context.load_table(TestTables::Simple).await?;
+    let file = "part-00003-53f42606-6cda-4f13-8d07-599a21197296-c000.snappy.parquet";
+    let path = Path::from_iter([&TestTables::Simple.as_name(), file]);
+
+    // Delete an active file from underlying storage without an update to the log to simulate an external fault
+    context.object_store().delete(&path).await?;
+
+    let table = context
+        .table_builder(TestTables::Simple)
+        .with_version(2)
+        .load()
+        .await?;
+
+    let op = DeltaOps::from(table);
+    let res = op.filesystem_check().with_dry_run(false).await;
+
+    // TODO check more specific error
+    assert!(matches!(res, Err(DeltaTableError::Transaction { .. })));
+
+    Ok(())
+}
+
+#[tokio::test]
+#[serial]
+#[ignore = "should this actually fail? with conflcit resolution, we are re-trying again."]
+async fn test_filesystem_check_outdated() -> TestResult {
+    // Validate failure when a non dry only executes on the latest version
+    let context = IntegrationContext::new(StorageIntegration::Local)?;
+    context.load_table(TestTables::Simple).await?;
+    let file = "part-00007-3a0e4727-de0d-41b6-81ef-5223cf40f025-c000.snappy.parquet";
+    let path = Path::from_iter([&TestTables::Simple.as_name(), file]);
+
+    // Delete an active file from underlying storage without an update to the log to simulate an external fault
+    context.object_store().delete(&path).await?;
+
+    let table = context
+        .table_builder(TestTables::Simple)
+        .with_version(2)
+        .load()
+        .await?;
+
+    let op = DeltaOps::from(table);
+    let res = op.filesystem_check().with_dry_run(false).await;
+    println!("{:?}", res);
+    if let Err(DeltaTableError::VersionAlreadyExists(version)) = res {
+        assert!(version == 3);
+    } else {
+        panic!();
+    }
+
+    Ok(())
+}
