b53b6865f1b62aeffa96fc47426ac29369743901
Ion Koutsouris <15728914+ion-elgreco@users.noreply.github.com>
2023-10-30 04:05:06 +0100

    feat(python): expose FSCK (repair) operation  (#1730)
    # Description
    This PR exposes the FSCK operation as a `repair` method under the
    `DeltaTable `class.
    
    # Related Issue(s)
    <!---
    For example:
    
    - closes #106
    --->
    - closes #1727
    
    ---------
    
    Co-authored-by: Will Jones <willjones127@gmail.com>

--- python/deltalake/table.py
+++ python/deltalake/table.py
@@ -826,6 +826,31 @@ class DeltaTable:
         metrics = self._table.delete(predicate)
         return json.loads(metrics)
 
+    def repair(self, dry_run: bool = False) -> Dict[str, Any]:
+        """Repair the Delta Table by auditing active files that do not exist in the underlying
+        filesystem and removes them. This can be useful when there are accidental deletions or corrupted files.
+
+        Active files are ones that have an add action in the log, but no corresponding remove action.
+        This operation creates a new FSCK transaction containing a remove action for each of the missing
+        or corrupted files.
+
+        Args:
+            dry_run(bool): when activated, list only the files, otherwise add remove actions to transaction log. Defaults to False.
+        Returns:
+            The metrics from repair (FSCK) action.
+
+        Examples:
+
+        >>> from deltalake import DeltaTable
+        >>> dt = DeltaTable('TEST')
+        >>> dt.repair(dry_run=False)
+        {'dry_run': False,
+        'files_removed': ['6-0d084325-6885-4847-b008-82c1cf30674c-0.parquet',
+        '5-4fba1d3e-3e20-4de1-933d-a8e13ac59f53-0.parquet']}
+        """
+        metrics = self._table.repair(dry_run)
+        return json.loads(metrics)
+
 
 class TableMerger:
     """API for various table MERGE commands."""
