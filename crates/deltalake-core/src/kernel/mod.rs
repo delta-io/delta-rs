0b60ea47148a5cafe8a653a0d50588a29d60e29f
Robert Pack <robstar.pack@gmail.com>
2023-11-09 11:21:04 +0100

    chore: add expressions from kernel

--- crates/deltalake-core/src/kernel/mod.rs
+++ crates/deltalake-core/src/kernel/mod.rs
@@ -4,8 +4,10 @@ pub mod actions;
 #[cfg(all(feature = "arrow", feature = "parquet"))]
 pub mod arrow;
 pub mod error;
+pub mod expressions;
 pub mod schema;
 
 pub use actions::*;
 pub use error::*;
+pub use expressions::*;
 pub use schema::*;
