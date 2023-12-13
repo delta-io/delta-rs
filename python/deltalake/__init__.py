a4fc4dc93228256683bfbc962751c70a707e858c
ion-elgreco <15728914+ion-elgreco@users.noreply.github.com>
2023-12-14 00:27:35 +0100

    move into table

--- python/deltalake/__init__.py
+++ python/deltalake/__init__.py
@@ -1,5 +1,3 @@
-import pyarrow_hotfix  # noqa: F401; addresses CVE-2023-47248; # type: ignore
-
 from ._internal import __version__ as __version__
 from ._internal import rust_core_version as rust_core_version
 from .data_catalog import DataCatalog as DataCatalog
