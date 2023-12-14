2d65e7281e22632c0d6e10cfb7b382dc6d9eee85
Stephen Carman <hntd187@users.noreply.github.com>
2023-12-14 04:17:42 -0500

    feat: check constraints (#1915)
    # Description
    This PR adds CHECK constraints on delta tables. I still have some
    outstanding work to do on this, but this is my working draft PR for this
    feature.
    
    ```rust
    let constraint = table.add_constraint().with_constraint("id", "id < 100");
    constraint.await?;
    ```
    
    # Related Issue(s)
    #1881 
    
    # Documentation
    
    <!---
    Share links to useful documentation
    --->
    
    ---------
    
    Co-authored-by: Stephen Carman <stephen.carman@databricks.com>
    Co-authored-by: scarman-db <106979814+scarman-db@users.noreply.github.com>

--- crates/deltalake-core/src/kernel/mod.rs
+++ crates/deltalake-core/src/kernel/mod.rs
@@ -13,3 +13,11 @@ pub use actions::*;
 pub use error::*;
 pub use expressions::*;
 pub use schema::*;
+
+/// A trait for all kernel types that are used as part of data checking
+pub trait DataCheck {
+    /// The name of the specific check
+    fn get_name(&self) -> &str;
+    /// The SQL expression to use for the check
+    fn get_expression(&self) -> &str;
+}
