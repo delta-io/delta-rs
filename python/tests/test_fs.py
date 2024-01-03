6f7a842c126510a88e5b1e08a11474c1e4c3ff34
R. Tyler Croy <rtyler@brokenco.de>
2024-01-03 10:51:03 -0800

    Temporarily skip Azure tests

--- python/tests/test_fs.py
+++ python/tests/test_fs.py
@@ -172,6 +172,7 @@ def test_roundtrip_s3_direct(s3_localstack_creds, sample_data: pa.Table):
 
 
 @pytest.mark.azure
+@pytest.mark.skip
 @pytest.mark.integration
 @pytest.mark.timeout(timeout=60, method="thread")
 def test_roundtrip_azure_env(azurite_env_vars, sample_data: pa.Table):
@@ -194,6 +195,7 @@ def test_roundtrip_azure_env(azurite_env_vars, sample_data: pa.Table):
 
 
 @pytest.mark.azure
+@pytest.mark.skip
 @pytest.mark.integration
 @pytest.mark.timeout(timeout=60, method="thread")
 def test_roundtrip_azure_direct(azurite_creds, sample_data: pa.Table):
@@ -216,6 +218,7 @@ def test_roundtrip_azure_direct(azurite_creds, sample_data: pa.Table):
 
 
 @pytest.mark.azure
+@pytest.mark.skip
 @pytest.mark.integration
 @pytest.mark.timeout(timeout=60, method="thread")
 def test_roundtrip_azure_sas(azurite_sas_creds, sample_data: pa.Table):
@@ -229,6 +232,7 @@ def test_roundtrip_azure_sas(azurite_sas_creds, sample_data: pa.Table):
 
 
 @pytest.mark.azure
+@pytest.mark.skip
 @pytest.mark.integration
 @pytest.mark.timeout(timeout=60, method="thread")
 def test_roundtrip_azure_decoded_sas(azurite_sas_creds, sample_data: pa.Table):
