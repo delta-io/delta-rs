8f938edd92319728f27750453a8f1c5558f18304
ion-elgreco <15728914+ion-elgreco@users.noreply.github.com>
2024-01-28 14:01:21 +0100

    drop custom filesystem

--- python/tests/test_benchmark.py
+++ python/tests/test_benchmark.py
@@ -35,19 +35,6 @@ def test_benchmark_write(benchmark, sample_table, tmp_path, engine):
     assert dt.to_pyarrow_table().sort_by("i") == sample_table
 
 
-# TODO: support wrapping PyArrow filesystems
-# @pytest.mark.benchmark(
-#     group="write"
-# )
-# def test_benchmark_write_pyarrow(benchmark, sample_table, tmp_path):
-#     fs = pa_fs.SubTreeFileSystem(str(tmp_path), pa_fs.LocalFileSystem())
-
-#     benchmark(write_deltalake, str(tmp_path), sample_table, mode="overwrite", filesystem=fs)
-
-#     dt = DeltaTable(str(tmp_path))
-#     assert dt.to_pyarrow_table(filesystem=fs).sort_by("i") == sample_table
-
-
 @pytest.mark.benchmark(group="read")
 def test_benchmark_read(benchmark, sample_table, tmp_path):
     write_deltalake(str(tmp_path), sample_table)
