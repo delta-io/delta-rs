444b4a797e1ac6fbc470ce493ee454545cb1ecb6
ion-elgreco <15728914+ion-elgreco@users.noreply.github.com>
2024-03-08 19:11:57 +0100

    add tests for create

--- python/tests/test_create.py
+++ python/tests/test_create.py
@@ -60,3 +60,63 @@ def test_create_schema(tmp_path: pathlib.Path, sample_data: pa.Table):
     )
 
     assert dt.schema().to_pyarrow() == sample_data.schema
+
+
+def test_create_with_deletion_vectors_enabled(
+    tmp_path: pathlib.Path, sample_table: pa.Table
+):
+    dt = DeltaTable.create(
+        tmp_path,
+        sample_table.schema,
+        name="test_name",
+        description="test_desc",
+        configuration={
+            "delta.appendOnly": "false",
+            "delta.enableDeletionVectors": "true",
+        },
+        custom_metadata={"userName": "John Doe"},
+    )
+
+    metadata = dt.metadata()
+    protocol = dt.protocol()
+    assert metadata.name == "test_name"
+    assert metadata.description == "test_desc"
+    assert metadata.configuration == {
+        "delta.appendOnly": "false",
+        "delta.enableDeletionVectors": "true",
+    }
+    assert protocol.min_reader_version == 3
+    assert protocol.min_writer_version == 7
+    assert set(protocol.writer_features) == {"deletionVectors", "appendOnly"}  # type: ignore
+    assert protocol.reader_features == ["deletionVectors"]
+    assert dt.history()[0]["userName"] == "John Doe"
+
+
+def test_create_higher_protocol_versions(
+    tmp_path: pathlib.Path, sample_table: pa.Table
+):
+    dt = DeltaTable.create(
+        tmp_path,
+        sample_table.schema,
+        name="test_name",
+        description="test_desc",
+        configuration={
+            "delta.appendOnly": "false",
+            "delta.minReaderVersion": "2",
+            "delta.minWriterVersion": "5",
+        },
+        custom_metadata={"userName": "John Doe"},
+    )
+
+    metadata = dt.metadata()
+    protocol = dt.protocol()
+    assert metadata.name == "test_name"
+    assert metadata.description == "test_desc"
+    assert metadata.configuration == {
+        "delta.appendOnly": "false",
+        "delta.minReaderVersion": "2",
+        "delta.minWriterVersion": "5",
+    }
+    assert protocol.min_reader_version == 2
+    assert protocol.min_writer_version == 5
+    assert dt.history()[0]["userName"] == "John Doe"
