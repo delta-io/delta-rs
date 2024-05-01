f6d110815c9a9eb0b10236ead92b763af0c16fa6
R. Tyler Croy <rtyler@brokenco.de>
2024-05-01 20:25:25 +0000

    chore: update the python version and dependencies for release

--- python/src/lib.rs
+++ python/src/lib.rs
@@ -1739,7 +1739,7 @@ fn _internal(py: Python, m: &PyModule) -> PyResult<()> {
     deltalake::aws::register_handlers(None);
     deltalake::azure::register_handlers(None);
     deltalake::gcp::register_handlers(None);
-    deltalake::mount::register_handlers(None);
+    deltalake_mount::register_handlers(None);
 
     m.add("DeltaError", py.get_type::<DeltaError>())?;
     m.add("CommitFailedError", py.get_type::<CommitFailedError>())?;
