b53c9143ec4988c102eaba4b267f946af26fd410
Abdullahsab3 <abdallah-sab3@hotmail.com>
2024-12-29 12:24:35 +0100

    fix outdated constraint example
    Signed-off-by: Abdullahsab3 <abdallah-sab3@hotmail.com>

--- docs/src/rust/check_constraints.rs
+++ docs/src/rust/check_constraints.rs
@@ -6,7 +6,7 @@ async fn main() -> Result<(), Box<dyn std::error::Error>> {
     // --8<-- [start:add_constraint]
     let table = deltalake::open_table("../rust/tests/data/simple_table").await?;
     let ops = DeltaOps(table);
-    ops.with_constraint("id_gt_0", "id > 0").await?;
+    ops.add_constraint().with_constraint("id_gt_0", "id > 0").await?;
     // --8<-- [end:add_constraint]
 
     // --8<-- [start:add_data]
