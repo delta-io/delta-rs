cee357af254f2bd2cde942f7de5ab470eacb30fd
R. Tyler Croy <rtyler@brokenco.de>
2025-01-03 02:10:20 +0000

    feat: allow multiple Python threads to work with a single DeltaTable instance
    This change introduces an internal Mutex inside of RawDeltaTable which
    allows the PyO3 bindings to share the Python object between threads at
    the Python layer.
    
    PyO3 will raise a `RuntimeError: Already borrowed` for any
    function call which takes a mutable reference to `self`. Introducing the
    internal Mutex ensures that all function signatures can operate with
    just self-references safely.
    
    The Rust-level Mutex is a simple passthrough for most operations which
    do not need to modify the underlying state. The critical sections which
    typically need to acquire and mutate with a lock are after I/O bound
    operations are completed as far as I can tell, so I don't anticipate
    deadlock or performance issues.
    
    There is still some cleanup of errors that needs to happen to make the
    code here more ergonomic when blending DeltaError with PoisonError from
    the lock, as such right now there's a lot of ugly error mapping.
    
    Fixes #2958
    
    Signed-off-by: R. Tyler Croy <rtyler@brokenco.de>
    Sponsored-by: Neuralink Corp.
    Signed-off-by: Rush <justin.rush00@delta.com>

--- python/src/query.rs
+++ python/src/query.rs
@@ -35,15 +35,15 @@ impl PyQueryBuilder {
     /// Once called, the provided `delta_table` will be referencable in SQL queries so long as
     /// another table of the same name is not registered over it.
     pub fn register(&self, table_name: &str, delta_table: &RawDeltaTable) -> PyResult<()> {
-        let snapshot = delta_table._table.snapshot().map_err(PythonError::from)?;
-        let log_store = delta_table._table.log_store();
+        let snapshot = delta_table.cloned_state()?;
+        let log_store = delta_table.log_store()?;
 
         let scan_config = DeltaScanConfigBuilder::default()
-            .build(snapshot)
+            .build(&snapshot)
             .map_err(PythonError::from)?;
 
         let provider = Arc::new(
-            DeltaTableProvider::try_new(snapshot.clone(), log_store, scan_config)
+            DeltaTableProvider::try_new(snapshot, log_store, scan_config)
                 .map_err(PythonError::from)?,
         );
 
