ec37821f16e423c827cafa676449340b9ffd05b0
Ion Koutsouris <15728914+ion-elgreco@users.noreply.github.com>
2025-03-01 16:37:15 +0100

    chore: import paths
    Signed-off-by: Ion Koutsouris <15728914+ion-elgreco@users.noreply.github.com>

--- python/deltalake/__init__.py
+++ python/deltalake/__init__.py
@@ -1,23 +1,23 @@
-from ._internal import TableFeatures as TableFeatures
-from ._internal import __version__ as __version__
-from ._internal import rust_core_version as rust_core_version
-from .query import QueryBuilder as QueryBuilder
-from .schema import DataType as DataType
-from .schema import Field as Field
-from .schema import Schema as Schema
-from .table import (
+from deltalake._internal import TableFeatures as TableFeatures
+from deltalake._internal import __version__ as __version__
+from deltalake._internal import rust_core_version as rust_core_version
+from deltalake.query import QueryBuilder as QueryBuilder
+from deltalake.schema import DataType as DataType
+from deltalake.schema import Field as Field
+from deltalake.schema import Schema as Schema
+from deltalake.table import (
     BloomFilterProperties as BloomFilterProperties,
 )
-from .table import (
+from deltalake.table import (
     ColumnProperties as ColumnProperties,
 )
-from .table import CommitProperties as CommitProperties
-from .table import DeltaTable as DeltaTable
-from .table import Metadata as Metadata
-from .table import PostCommitHookProperties as PostCommitHookProperties
-from .table import Transaction as Transaction
-from .table import (
+from deltalake.table import CommitProperties as CommitProperties
+from deltalake.table import DeltaTable as DeltaTable
+from deltalake.table import Metadata as Metadata
+from deltalake.table import PostCommitHookProperties as PostCommitHookProperties
+from deltalake.table import Transaction as Transaction
+from deltalake.table import (
     WriterProperties as WriterProperties,
 )
-from .writer import convert_to_deltalake as convert_to_deltalake
-from .writer import write_deltalake as write_deltalake
+from deltalake.writer import convert_to_deltalake as convert_to_deltalake
+from deltalake.writer import write_deltalake as write_deltalake
