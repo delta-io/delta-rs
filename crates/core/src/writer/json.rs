93b510d6c7ca1991579607c32edf197138aaed2b
R. Tyler Croy <rtyler@brokenco.de>
2025-03-08 18:02:32 +0000

    fix: modify tests which compare partition values due to breakage in kernel
    as of delta-kernel-rs 0.7.0 a Vec<Scalar> will no longer be comparable
    if there are nulls in it, which breaks a couple of our tests which
    perform said check.
    
    I feel that this is likely going to cause downstream issues with users
    of delta-rs, in which case we will have to bring it up with the kernel
    developers who assert that Null(String) will never equal Null(String).
    
    Signed-off-by: R. Tyler Croy <rtyler@brokenco.de>

--- crates/core/src/writer/json.rs
+++ crates/core/src/writer/json.rs
@@ -542,18 +542,13 @@ mod tests {
 
         assert_eq!(
             extract_partition_values(
-                &[
-                    String::from("col1"),
-                    String::from("col2"),
-                    String::from("col3")
-                ],
+                &[String::from("col1"), String::from("col2"),],
                 &record_batch
             )
             .unwrap(),
             IndexMap::from([
                 (String::from("col1"), Scalar::Integer(1)),
                 (String::from("col2"), Scalar::Integer(2)),
-                (String::from("col3"), Scalar::Null(DataType::INTEGER)),
             ])
         );
         assert_eq!(
