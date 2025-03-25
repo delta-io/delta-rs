a5fac157912eff563d621707a94f6df28f3ed820
Ze'ev Maor <zeevm@outlook.com>
2025-03-25 14:49:39 +0200

    Fix UT
    Signed-off-by: Ze'ev Maor <zeevm@outlook.com>

--- crates/core/tests/read_delta_log_test.rs
+++ crates/core/tests/read_delta_log_test.rs
@@ -185,10 +185,9 @@ async fn read_delta_table_with_null_stats_in_notnull_struct() {
 
 #[tokio::test]
 async fn read_delta_table_with_renamed_partitioning_column() {
-    let table =
-        deltalake_core::open_table("../test/tests/data/table_with_partitioning_mapping")
-            .await
-            .unwrap();
-    assert_eq!(table.version(), 1);
+    let table = deltalake_core::open_table("../test/tests/data/table_with_partitioning_mapping")
+        .await
+        .unwrap();
+    assert_eq!(table.version(), 4);
     assert!(table.get_schema().is_ok());
 }
