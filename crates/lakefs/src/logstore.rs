e5a7963f9660f3ffae03baf700942f974c44a622
Robert Pack <robstar.pack@gmail.com>
2025-05-29 14:13:30 +0200

    chore!: remove get_earliest_version
    Signed-off-by: Robert Pack <robstar.pack@gmail.com>

--- crates/lakefs/src/logstore.rs
+++ crates/lakefs/src/logstore.rs
@@ -357,10 +357,6 @@ impl LogStore for LakeFSLogStore {
         get_latest_version(self, current_version).await
     }
 
-    async fn get_earliest_version(&self, current_version: i64) -> DeltaResult<i64> {
-        get_earliest_version(self, current_version).await
-    }
-
     fn object_store(&self, operation_id: Option<Uuid>) -> Arc<dyn ObjectStore> {
         match operation_id {
             Some(id) => {
