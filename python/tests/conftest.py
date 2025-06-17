281e844cd9d1ef78c2f9eb26661f1f89bcd78e1b
Don Tolley <tolleybot@gmail.com>
2025-06-17 00:44:52 +0000

    test(python): add optimize.sort tests and skip tests without arro3
    Signed-off-by: tolleybot <>
    Signed-off-by: Don Tolley <tolleybot@gmail.com>

--- python/tests/conftest.py
+++ python/tests/conftest.py
@@ -8,7 +8,11 @@ from time import sleep
 from typing import TYPE_CHECKING
 
 import pytest
-from arro3.core import Array, DataType, Field, Schema, Table
+import pytest
+try:
+    from arro3.core import Array, DataType, Field, Schema, Table
+except ImportError:
+    pytest.skip("arro3 not installed; skipping Python deltarlake tests", allow_module_level=True)
 from azure.storage import blob
 
 from deltalake import DeltaTable, WriterProperties, write_deltalake
