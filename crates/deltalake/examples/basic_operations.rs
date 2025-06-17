6f2fb284c5965454c6c912efb6fb79456a2f52b8
Corwin Joy <corwin.joy@gmail.com>
2025-06-16 19:13:20 -0700

    Restore basic_operations.rs
    Signed-off-by: Corwin Joy <corwin.joy@gmail.com>

--- crates/deltalake/examples/basic_operations.rs
+++ crates/deltalake/examples/basic_operations.rs
@@ -1,11 +1,10 @@
-use std::collections::{HashMap, HashSet};
-use std::fs;
 use deltalake::arrow::{
     array::{Int32Array, StringArray, TimestampMicrosecondArray},
     datatypes::{DataType as ArrowDataType, Field, Schema as ArrowSchema, TimeUnit},
     record_batch::RecordBatch,
 };
 use deltalake::kernel::{DataType, PrimitiveType, StructField};
+use deltalake::operations::collect_sendable_stream;
 use deltalake::parquet::{
     basic::{Compression, ZstdLevel},
     file::properties::WriterProperties,
@@ -65,15 +64,11 @@ fn get_table_batches() -> RecordBatch {
 #[tokio::main(flavor = "current_thread")]
 async fn main() -> Result<(), deltalake::errors::DeltaTableError> {
     // Create a delta operations client pointing at an un-initialized location.
-    let path = "/home/cjoy/src/delta-rs/crates/deltalake/examples/test_v";
-    let joined = String::from("file://") + path;
-    let table_uri = joined.as_str();
-
-    let _ = fs::remove_dir_all(path);
-    fs::create_dir(path)?;
-    
-    let ops = DeltaOps::try_from_uri(table_uri).await?
-    ;
+    let ops = if let Ok(table_uri) = std::env::var("TABLE_URI") {
+        DeltaOps::try_from_uri(table_uri).await?
+    } else {
+        DeltaOps::new_in_memory()
+    };
 
     // The operations module uses a builder pattern that allows specifying several options
     // on how the command behaves. The builders implement `Into<Future>`, so once
@@ -81,7 +76,7 @@ async fn main() -> Result<(), deltalake::errors::DeltaTableError> {
     let table = ops
         .create()
         .with_columns(get_table_columns())
-        // .with_partition_columns(["timestamp"])
+        .with_partition_columns(["timestamp"])
         .with_table_name("my_table")
         .with_comment("A table to show how delta-rs works")
         .await?;
@@ -108,106 +103,15 @@ async fn main() -> Result<(), deltalake::errors::DeltaTableError> {
     let table = DeltaOps(table)
         .write(vec![batch.clone()])
         .with_save_mode(SaveMode::Overwrite)
-        .with_writer_properties(writer_properties.clone())
-        .await?;
-
-    assert_eq!(table.version(), Some(2));
-    
-    // To append data, you can use the same `write` method again:
-    let table = DeltaOps(table)
-        .write(vec![batch.clone()])
         .with_writer_properties(writer_properties)
         .await?;
 
-    assert_eq!(table.version(), Some(3));
-    
-    // TODO show history
-    let mut history = table
-        .history(None)
-        .await?;
-    
-    history.reverse();
-    
-    println!("###############################################################################");
-    println!("history:");
-    for (version, action) in history.iter().enumerate() {
-        println!("version:{}\n, {:#?}", version, action);
-    }
-    
-    let tombstones = table.snapshot()
-        .unwrap()
-        .all_tombstones(&table.log_store())
-        .await
-        .unwrap()
-        .collect::<Vec<_>>();
+    assert_eq!(table.version(), Some(2));
 
-    println!("###############################################################################");
-    println!("tombstones:");
-    println!("{:#?}", tombstones);
-    
-    let tombstone_files = table
-        .snapshot()
-        .unwrap()
-        .all_tombstones(&table.log_store())
-        .await
-        .unwrap()
-        .map(|r| r.path.clone())
-        .collect::<Vec<_>>();
+    let (_table, stream) = DeltaOps(table).load().await?;
+    let data: Vec<RecordBatch> = collect_sendable_stream(stream).await?;
 
-    println!("###############################################################################");
-    println!("tombstone_files:");
-    println!("{:#?}", tombstone_files);
-    
-    let max_version = table
-        .snapshot()?
-        .version();
-
-    println!("###############################################################################");
-    println!("all active files used by version:");
-    let mut files_by_version: HashMap<i64, Vec<String>> = HashMap::new();
-    let mut tv = table.clone();
-    tv.state = None;
-    
-    
-    for i in 0..=max_version {
-        tv.update_incremental(Some(i)).await?;
-        let files: Vec<String> = tv.get_files_iter()?.map(|path| path.to_string()).collect();
-        println!("version:{}\n, {:#?}", i, files);
-        files_by_version.insert(i, files);
-    }
-    
-    let versions_to_keep = vec![3];
-    let mut keep_files = HashSet::new();
-    for version in versions_to_keep.iter() {
-        if let Some(files) = files_by_version.get(&version) {
-            for file in files {
-                keep_files.insert(file.clone());
-            }
-        }
-    }
-
-    println!("###############################################################################");
-    println!("keep file list:");
-    println!("versions_to_keep:{:#?}", versions_to_keep);
-    println!("keep_files:{:#?}", keep_files);
-    
-    // Vacuum
-    println!("###############################################################################");
-    println!("vacuum:");
-    let (_, metrics) = DeltaOps(table).vacuum()
-        .with_retention_period(chrono::Duration::seconds(0))
-        .with_enforce_retention_duration(false)
-        .with_keep_versions(versions_to_keep)
-        .await?;
-    println!("###############################################################################");
-    println!("vacuum metrics:");
-    println!("{:#?}", metrics);
-
-
-    //let (_table, stream) = DeltaOps(table).load().await?;
-    //let data: Vec<RecordBatch> = collect_sendable_stream(stream).await?;
-
-    //println!("{data:?}");
+    println!("{data:?}");
 
     Ok(())
 }
