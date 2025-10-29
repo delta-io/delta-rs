8199d20ef202a4b0c15fdc92bfebf8f87da0e194
Abhi Agarwal <abhi@airspace-intelligence.com>
2025-10-28 20:34:04 -0400

    type signature
    Signed-off-by: Abhi Agarwal <abhi@airspace-intelligence.com>

--- python/src/lib.rs
+++ python/src/lib.rs
@@ -2376,7 +2376,7 @@ fn write_to_deltalake(
     schema_mode: Option<String>,
     partition_by: Option<Vec<String>>,
     predicate: Option<String>,
-    target_file_size: Option<usize>,
+    target_file_size: Option<u64>,
     name: Option<String>,
     description: Option<String>,
     configuration: Option<HashMap<String, Option<String>>>,
