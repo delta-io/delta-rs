609155045c08b5dbfdf1f43ab71e20d173c7166e
Robert Pack <42610831+roeap@users.noreply.github.com>
2025-12-20 09:31:06 +0100

    refactor: simplify kernel extensions (#4003)
    # Description
    
    This PR removes one of these things that I added b/c we **probably**
    need it, well. turns out we don't, so lets get rid of the associated
    warnings for unused code.
    
    API-wise it's all internal.
    
    Signed-off-by: Robert Pack <robstar.pack@gmail.com>

--- crates/core/src/kernel/snapshot/mod.rs
+++ crates/core/src/kernel/snapshot/mod.rs
@@ -42,7 +42,7 @@ use serde_json::Deserializer;
 use url::Url;
 
 use super::{Action, CommitInfo, Metadata, Protocol};
-use crate::kernel::arrow::engine_ext::{ExpressionEvaluatorExt, kernel_to_arrow};
+use crate::kernel::arrow::engine_ext::{ExpressionEvaluatorExt, rb_from_scan_meta};
 use crate::kernel::{ARROW_HANDLER, StructType, spawn_blocking_with_span};
 use crate::logstore::{LogStore, LogStoreExt};
 use crate::{DeltaResult, DeltaTableConfig, DeltaTableError, PartitionFilter, to_kernel_predicate};
@@ -257,7 +257,7 @@ impl Snapshot {
         let engine = log_store.engine(None);
         let stream = scan
             .scan_metadata(engine)
-            .map(|d| Ok(kernel_to_arrow(d?)?.scan_files));
+            .map(|d| Ok(rb_from_scan_meta(d?)?));
 
         ScanRowOutStream::new(self.inner.clone(), stream).boxed()
     }
@@ -278,7 +278,7 @@ impl Snapshot {
         let engine = log_store.engine(None);
         let stream = scan
             .scan_metadata_from(engine, existing_version, existing_data, existing_predicate)
-            .map(|d| Ok(kernel_to_arrow(d?)?.scan_files));
+            .map(|d| Ok(rb_from_scan_meta(d?)?));
 
         ScanRowOutStream::new(self.inner.clone(), stream).boxed()
     }
