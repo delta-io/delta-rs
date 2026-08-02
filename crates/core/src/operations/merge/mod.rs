3e1d3717b8191cd882987267671267b4fde7b9e9
MedHeikelBouzayene <mohamed-heikel.bouzayene@outlook.com>
2026-08-02 11:01:06 +0100

    fix: add mergeSchema to rust test
    Signed-off-by: MedHeikelBouzayene <mohamed-heikel.bouzayene@outlook.com>

--- crates/core/src/operations/merge/mod.rs
+++ crates/core/src/operations/merge/mod.rs
@@ -2107,6 +2107,7 @@ mod tests {
             .merge(source, predicate)
             .with_source_alias("source")
             .with_target_alias("target")
+            .with_merge_schema(true)
             .when_matched_update(|update| {
                 update
                     .update("id", col("source.id"))
